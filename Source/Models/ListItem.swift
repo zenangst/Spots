fdcdd1dbb0d1cab71b5ef3de0d530001cc67be78
Christoffer Winterkvist <christoffer@hyper.no>
2015-10-05 00:17:03 +0200

    Make ListItem mappable

--- Source/Models/ListItem.swift
+++ Source/Models/ListItem.swift
@@ -1,18 +1,18 @@
 import Tailor
 import Sugar
 
-struct ListItem {
+struct ListItem: Mappable {
   var title = ""
   var subtitle = ""
   var image = ""
   var type = ""
   var uri: String?
 
-  init(json: JSONDictionary) {
-    self.title <- json.property("title")
-    self.subtitle <- json.property("subtitle")
-    self.image <- json.property("image")
-    self.type <- json.property("type")
-    self.uri <- json.property("uri")
+  init(_ map: JSONDictionary) {
+    self.title <- map.property("title")
+    self.subtitle <- map.property("subtitle")
+    self.image <- map.property("image")
+    self.type <- map.property("type")
+    self.uri <- map.property("uri")
   }
 }
