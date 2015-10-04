78fd7a0d7f251d09035f13b2c60cb2a5fa4ee84b
Christoffer Winterkvist <christoffer@hyper.no>
2015-10-04 20:18:20 +0200

    Move ListItem into new file

--- Source/Components/ListComponent.swift
+++ Source/Components/ListComponent.swift
@@ -6,22 +6,6 @@ protocol ComponentView {
   func render() -> UIView
 }
 
-struct ListItem {
-  var title = ""
-  var subtitle = ""
-  var image = ""
-  var type = ""
-  var uri: String?
-  
-  init(json: JSONDictionary) {
-    self.title <- json.property("title")
-    self.subtitle <- json.property("subtitle")
-    self.image <- json.property("image")
-    self.type <- json.property("type")
-    self.uri <- json.property("uri")
-  }
-}
-
 class ListComponent: NSObject, ComponentView {
 
   lazy var tableView: UITableView = { [unowned self] in
