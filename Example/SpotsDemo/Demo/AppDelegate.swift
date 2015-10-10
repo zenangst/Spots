f3790b6c3ca1a1a68d80a4fec423ca66a823f21d
Christoffer Winterkvist <christoffer@hyper.no>
2015-10-10 20:07:14 +0200

    Add styling

--- Example/SpotsDemo/Demo/AppDelegate.swift
+++ Example/SpotsDemo/Demo/AppDelegate.swift
@@ -17,9 +17,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     navigationController = UINavigationController(rootViewController: controller)
     window?.rootViewController = navigationController
+
+    applyStyles()
+    
     window?.makeKeyAndVisible()
     
     return true
+  }
+
+  func applyStyles() {
+    UIApplication.sharedApplication().statusBarStyle = .LightContent
+
+    let navigationBar = UINavigationBar.appearance()
+    navigationBar.barTintColor = UIColor(red:0.000, green:0.000, blue:0.000, alpha: 1)
+    navigationBar.tintColor = UIColor(red:1.000, green:1.000, blue:1.000, alpha: 1)
+    navigationBar.shadowImage = UIImage()
+    navigationBar.titleTextAttributes = [
+      NSForegroundColorAttributeName: UIColor(red:1.000, green:1.000, blue:1.000, alpha: 1)
+    ]
   }
 }
 
