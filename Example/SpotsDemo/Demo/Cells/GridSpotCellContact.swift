fb817e27c7e13e19b77566fd9abb9854f38f1c48
Christoffer Winterkvist <christoffer@hyper.no>
2015-10-21 22:59:19 +0200

    Add custom cells

---
+++ Example/SpotsDemo/Demo/Cells/GridSpotCellContact.swift
@@ -0,0 +1,60 @@
+import UIKit
+import GoldenRetriever
+import Sugar
+
+class GridSpotCellContact : UICollectionViewCell, Itemble {
+
+  var size = CGSize(width: 88, height: 120)
+
+  lazy var imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .ScaleAspectFill
+    imageView.autoresizingMask = [.FlexibleWidth]
+    return imageView
+    }()
+
+  lazy var titleLabel: UILabel = { [unowned self] in
+    let label = UILabel(frame: self.contentView.frame)
+    label.textColor = UIColor.whiteColor()
+    label.textAlignment = .Center
+    label.autoresizingMask = [.FlexibleWidth]
+    label.font = UIFont(name: "Avenir Next", size: 12)
+    return label
+    }()
+
+  func configure(inout item: ListItem) {
+    if item.image != "" {
+      let resource = item.image
+      let fido = GoldenRetriever()
+      let qualityOfServiceClass = QOS_CLASS_BACKGROUND
+      let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
+
+      dispatch(backgroundQueue) {
+        fido.fetch(resource) { data, error in
+          guard let data = data else { return }
+          let image = UIImage(data: data)
+          dispatch {
+            self.imageView.image = image
+          }
+        }
+      }
+    }
+
+    if imageView.superview == nil {
+      contentView.addSubview(imageView)
+    }
+  }
+
+  override func layoutSubviews() {
+    imageView.frame = contentView.frame
+    imageView.frame.size.height = 88
+    imageView.frame.size.width = 88
+    imageView.frame.origin.x = frame.size.width / 2 - imageView.frame.size.width / 2
+    imageView.frame.origin.y = frame.size.height / 2 - imageView.frame.size.height / 2
+    imageView.clipsToBounds = true
+    imageView.layer.cornerRadius = 44
+    imageView.layer.borderColor = UIColor.whiteColor().CGColor
+    imageView.layer.borderWidth = 2.0
+  }
+
+}
---
+++ Example/SpotsDemo/Demo/Cells/GridSpotTitles.swift
@@ -0,0 +1,48 @@
+import UIKit
+import GoldenRetriever
+import Sugar
+
+class GridSpotCellTitles : UICollectionViewCell, Itemble {
+
+  var size = CGSize(width: 88, height: 120)
+
+  lazy var titleLabel: UILabel = { [unowned self] in
+    let label = UILabel(frame: self.contentView.frame)
+    label.textColor = UIColor.blackColor()
+    label.textAlignment = .Center
+    label.autoresizingMask = [.FlexibleWidth]
+    label.font = UIFont(name: "AvenirNext-Bold", size: 34)
+    label.numberOfLines = 2
+    return label
+    }()
+
+  lazy var subtitleLabel: UILabel = { [unowned self] in
+    let label = UILabel(frame: self.contentView.frame)
+    label.textColor = UIColor.blackColor()
+    label.textAlignment = .Center
+    label.autoresizingMask = [.FlexibleWidth]
+    label.font = UIFont(name: "SanFranciscoDisplay-Medium", size: 18)
+    label.textColor = UIColor(red:0.933, green:0.459, blue:0.200, alpha: 1)
+    label.numberOfLines = 3
+    return label
+    }()
+
+  func configure(inout item: ListItem) {
+    if titleLabel.superview == nil {
+      titleLabel.text = item.title
+      titleLabel.sizeToFit()
+      addSubview(titleLabel)
+    }
+
+    if subtitleLabel.superview == nil {
+      subtitleLabel.text = item.subtitle
+      subtitleLabel.sizeToFit()
+      subtitleLabel.frame.origin.y = titleLabel.frame.size.height + titleLabel.frame.origin.y
+      subtitleLabel.frame.size.width = contentView.frame.size.width
+      subtitleLabel.frame.size.height += 20
+      addSubview(subtitleLabel)
+    }
+
+    item.size.height = titleLabel.frame.height + subtitleLabel.frame.height
+  }
+}
