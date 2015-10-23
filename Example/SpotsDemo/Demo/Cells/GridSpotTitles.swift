e1fd3c2bc4936ed4c14ba97b8ef839dbf898cbc6
Christoffer Winterkvist <christoffer@hyper.no>
2015-10-23 09:24:28 +0200

    Refactor custom titles cell

--- Example/SpotsDemo/Demo/Cells/GridSpotTitles.swift
+++ Example/SpotsDemo/Demo/Cells/GridSpotTitles.swift
@@ -4,61 +4,77 @@ import Sugar
 
 class GridSpotCellTitles : UICollectionViewCell, Itemble {
 
-  var size = CGSize(width: 88, height: 120)
+  var size = CGSize(width: 88, height: 88)
 
   lazy var titleLabel: UILabel = { [unowned self] in
     let label = UILabel(frame: self.contentView.frame)
     label.textColor = UIColor.blackColor()
     label.textAlignment = .Center
     label.autoresizingMask = [.FlexibleWidth]
-    label.font = UIFont(name: "AvenirNext-Bold", size: 34)
+    label.font = UIFont(name: "AvenirNext-Bold", size: 30)
     label.numberOfLines = 2
     return label
     }()
 
   lazy var subtitleLabel: UILabel = { [unowned self] in
     let label = UILabel(frame: self.contentView.frame)
-    label.textColor = UIColor.blackColor()
     label.textAlignment = .Justified
-    label.autoresizingMask = [.FlexibleWidth]
     label.font = UIFont.systemFontOfSize(16)
     label.textColor = UIColor(red:0.933, green:0.459, blue:0.200, alpha: 1)
+    label.font = UIFont(name: "Georgia", size: 16)
     label.numberOfLines = 0
     return label
+    }()
+
+  lazy var metaText: UILabel = { [unowned self] in
+    let label = UILabel(frame: self.contentView.frame)
+    label.textAlignment = .Justified
+    label.font = UIFont.systemFontOfSize(14)
+    label.numberOfLines = 0
+    return label
+  }()
+
+  lazy var paddedStyle: NSParagraphStyle = {
+    let style = NSMutableParagraphStyle()
+    style.alignment = .Left
+    style.firstLineHeadIndent = 20.0
+    style.headIndent = 20.0
+    style.tailIndent = -20.0
+    return style
     }()
 
   func configure(inout item: ListItem) {
 
     if let textColor = item.meta["text-color"] {
       titleLabel.textColor = UIColor(hex: textColor)
-      subtitleLabel.textColor = UIColor(hex: textColor)
     }
 
-    if titleLabel.superview == nil {
-      titleLabel.text = item.title
-      contentView.addSubview(titleLabel)
-    }
+    titleLabel.text = item.title
+    subtitleLabel.attributedText = NSAttributedString(string: item.subtitle,
+      attributes: [NSParagraphStyleAttributeName : paddedStyle])
+    metaText.attributedText = NSAttributedString(string: item.meta.property("text") ?? "",
+      attributes: [NSParagraphStyleAttributeName : paddedStyle])
+    metaText.textColor = UIColor(hex: item.meta.property("text-color") ?? "000000")
 
-    if subtitleLabel.superview == nil {
-      subtitleLabel.text = item.subtitle
-      contentView.addSubview(subtitleLabel)
+    [titleLabel, subtitleLabel, metaText].forEach {
+      if $0.superview == nil { contentView.addSubview($0) }
     }
 
     layoutSubviews()
-    
-    item.size.height = subtitleLabel.frame.origin.y + subtitleLabel.frame.height
+
+    item.size.height = metaText.frame.origin.y + metaText.frame.height + 20
   }
 
   override func layoutSubviews() {
     super.layoutSubviews()
 
-    titleLabel.sizeToFit()
-    titleLabel.frame.size.width = contentView.frame.width
+    [titleLabel, subtitleLabel, metaText].forEach {
+      $0.sizeToFit()
+      $0.frame.size.width = contentView.frame.width
+    }
 
-    subtitleLabel.sizeToFit()
-    var size = subtitleLabel.frame.size
-    size.width -= 40
-    subtitleLabel.sizeThatFits(size)
-    subtitleLabel.frame.origin.y = titleLabel.frame.size.height + titleLabel.frame.origin.y
+    titleLabel.frame.origin.y = 10
+    subtitleLabel.frame.origin.y = titleLabel.frame.size.height + titleLabel.frame.origin.y + 10
+    metaText.frame.origin.y = subtitleLabel.frame.size.height + subtitleLabel.frame.origin.y + 10
   }
 }
