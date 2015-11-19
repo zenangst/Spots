6c094b16544a9fcf09bcb8e2965c73013f283708
Christoffer Winterkvist <christoffer@hyper.no>
2015-11-19 12:32:21 +0100

    Add methods to Spotable protocol

--- Source/Library/Spotable.swift
+++ Source/Library/Spotable.swift
@@ -9,7 +9,24 @@ public protocol Spotable: class {
 
   init(component: Component)
   func setup()
-  func reload()
+  func append(item: ListItem, completion: (() -> Void)?)
+  func append(items: [ListItem], completion: (() -> Void)?)
+  func insert(item: ListItem, index: Int, completion: (() -> Void)?)
+  func update(item: ListItem, index: Int, completion: (() -> Void)?)
+  func delete(index: Int, completion: (() -> Void)?)
+  func delete(indexes: [Int], completion: (() -> Void)?)
+  func reload(indexes: [Int], completion: (() -> Void)?)
   func render() -> UIView
   func layout(size: CGSize)
+}
+
+extension Spotable {
+
+  public func append(item: ListItem, completion: (() -> Void)? = nil) {}
+  public func append(items: [ListItem], completion: (() -> Void)? = nil) {}
+  public func insert(item: ListItem, index: Int, completion: (() -> Void)? = nil) {}
+  public func update(item: ListItem, index: Int, completion: (() -> Void)? = nil) {}
+  public func delete(index: Int, completion: (() -> Void)? = nil) {}
+  public func delete(indexs: [Int], completion: (() -> Void)? = nil) {}
+
 }
