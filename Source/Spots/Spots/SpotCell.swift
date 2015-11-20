25abdbd5dffe1292eb829f0fa8b322c2a1a1c4cb
Christoffer Winterkvist <christoffer@hyper.no>
2015-11-20 09:44:49 +0100

    Optimize feed spot

--- Source/Spots/Spots/SpotCell.swift
+++ Source/Spots/Spots/SpotCell.swift
@@ -14,6 +14,6 @@ public class SpotCell: UICollectionViewCell {
   }
 
   required public init?(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
+    fatalError("init(coder:) has not been implemented")
   }
 }
