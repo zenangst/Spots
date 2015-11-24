a625a2b65eba8f205b941dff6c77078db57c122e
Christoffer Winterkvist <christoffer@hyper.no>
2015-11-24 08:59:21 +0100

    Update SpotsDemo

--- Example/SpotsDemo/Demo/Cells/GridSpotHeader.swift
+++ Example/SpotsDemo/Demo/Cells/GridSpotHeader.swift
@@ -29,10 +29,8 @@ class GridSpotHeader : UICollectionViewCell, Itemble {
       imageView.image = nil
       let resource = item.image
       let fido = GoldenRetriever()
-      let qualityOfServiceClass = QOS_CLASS_BACKGROUND
-      let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
 
-      dispatch(backgroundQueue) {
+      dispatch(queue: .Interactive) {
         fido.fetch(resource) { data, error in
           guard let data = data else { return }
           let image = UIImage(data: data)
