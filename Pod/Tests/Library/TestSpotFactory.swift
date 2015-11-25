bfb5a561b5369349f68b973d9fe5ce976a079a63
Vadym Markov <markov.vadym@gmail.com>
2015-11-25 17:13:00 +0100

    Add Spot factory tests

---
+++ Pod/Tests/Library/TestSpotFactory.swift
@@ -0,0 +1,15 @@
+import Foundation
+import XCTest
+
+class SpotFactoryTests : XCTestCase {
+
+  var json: [String : AnyObject] = [
+    "title" : "title1",
+    "type" : "merry-go-round",
+    "span" : 1,
+    "meta" : ["foo" : "bar"],
+    "items" : [["title" : "item1"]]
+  ]
+
+  
+}
