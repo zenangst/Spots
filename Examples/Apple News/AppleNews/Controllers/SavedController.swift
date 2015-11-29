55c7ee03eee6d256b58d9a015e171c2c4a693cc0
Christoffer Winterkvist <christoffer@hyper.no>
2015-11-29 21:18:10 +0100

    Add SavedController

---
+++ Examples/Apple News/AppleNews/Controllers/SavedController.swift
@@ -0,0 +1,25 @@
+import Spots
+import Fakery
+import Sugar
+
+class SavedController: SpotsController {
+
+  static let faker = Faker()
+
+  convenience init(title: String) {
+    let component = Component()
+    let feedSpot = FeedSpot(component: component)
+    self.init(spots: [feedSpot], refreshable: false)
+
+    self.title = title
+    collectionView.scrollEnabled = false
+
+    dispatch(queue: .Interactive) { [weak self] in
+      let items = ForYouController.generateItems(0, to: 2)
+      self?.updateSpotAtIndex(0, closure: { (spot) -> Spotable in
+        spot.component.items = items
+        return spot
+      })
+    }
+  }
+}
