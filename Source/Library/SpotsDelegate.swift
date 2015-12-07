f1c50b6dcb97cbd59758dc93f5ce1c9ff04bcc02
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-07 01:29:07 +0100

    Add completion to spotsDidReload

--- Source/Library/SpotsDelegate.swift
+++ Source/Library/SpotsDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 public protocol SpotsDelegate: class {
 
   func spotDidSelectItem(spot: Spotable, item: ListItem)
-  func spotsDidReload(refreshControl: UIRefreshControl)
+  func spotsDidReload(refreshControl: UIRefreshControl, completion: (() -> Void)?)
   func spotDidReachEnd(completion: (() -> Void)?)
 }
 
