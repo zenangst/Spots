df477685db8005e6b2d3c2266723ae4ac3963f83
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-07 16:53:54 +0100

    Remove self

--- Source/Controllers/SpotsController.swift
+++ Source/Controllers/SpotsController.swift
@@ -101,7 +101,7 @@ public class SpotsController: UIViewController, UIScrollViewDelegate {
 
     delay(0.1) {
       delay(1.0) {
-        self.spotDelegate?.spotsDidReload(self.refreshControl) { [weak self] in
+        self.spotDelegate?.spotsDidReload(refreshControl) { [weak self] in
           UIView.animateWithDuration(0.3, animations: {
             self?.container.contentOffset = defaultContainerOffset
             self?.container.contentInset.top = defaultContainerInset
