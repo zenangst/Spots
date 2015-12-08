d9167279fe09b7b1832ee9fafecee2a83974377f
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-08 13:51:56 +0100

    Add self

--- Source/Controllers/SpotsController+UIScrollViewDelegate.swift
+++ Source/Controllers/SpotsController+UIScrollViewDelegate.swift
@@ -34,7 +34,7 @@ extension SpotsController {
     container.contentInset.top = -scrollView.contentOffset.y
 
     delay(0.5) {
-      self.spotDelegate?.spotsDidReload(refreshControl) { [weak self] in
+      self.spotDelegate?.spotsDidReload(self.refreshControl) { [weak self] in
         guard let weakSelf = self else { return }
         UIView.animateWithDuration(0.3, animations: {
           weakSelf.container.contentInset = weakSelf.initialContentInset
