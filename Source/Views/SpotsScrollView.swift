afe9032180337ecac0c06718f0c18b643bbad6eb
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-09 11:56:26 +0100

    Add logic for handling the resizing of UITableViews

--- Source/Views/SpotsScrollView.swift
+++ Source/Views/SpotsScrollView.swift
@@ -98,6 +98,14 @@ public class SpotsScrollView: UIScrollView {
           frame.origin.y = self.contentOffset.y
         }
 
+        // TODO: Fix this properly...
+        // This should also apply for UICollectionView but I haven't figured out a way to resize them properly without it going ape-shit over that the layout is incorrect.
+        if subview is UITableView {
+          let remainingBoundsHeight = fmax(CGRectGetMaxY(bounds) - CGRectGetMinY(frame), 0.0)
+          let remainingContentHeight = fmax(scrollView.contentSize.height - contentOffset.y, 0.0)
+          frame.size.height = ceil(fmin(remainingBoundsHeight, remainingContentHeight))
+        }
+
         frame.size.width = ceil(contentView.frame.size.width)
 
         scrollView.frame = frame
@@ -118,7 +126,11 @@ public class SpotsScrollView: UIScrollView {
     let initialContentOffset = contentOffset
     contentSize = CGSize(width: bounds.size.width, height: fmax(yOffsetOfCurrentSubview, minimumContentHeight))
 
+    print("contentSize: \(contentSize)")
+    print("contentInset: \(contentInset)")
+
     if initialContentOffset != contentOffset {
+      print("setNeedsLayout")
       setNeedsLayout()
       layoutIfNeeded()
     }
