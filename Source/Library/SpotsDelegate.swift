37de063768abcd4ecb882d455dea1b4a0c6e5494
Vadym Markov <markov.vadym@gmail.com>
2015-12-10 00:17:54 +0100

    Add SpotsScrollDelegate

--- Source/Library/SpotsDelegate.swift
+++ Source/Library/SpotsDelegate.swift
@@ -3,10 +3,15 @@ import UIKit
 public protocol SpotsDelegate: class {
 
   func spotDidSelectItem(spot: Spotable, item: ListItem)
+}
+
+public protocol SpotsScrollDelegate: class {
+
   func spotsDidReload(refreshControl: UIRefreshControl, completion: (() -> Void)?)
   func spotDidReachEnd(completion: (() -> Void)?)
 }
 
 extension SpotsDelegate {
+
   public func spotDidReachEnd(completion: (() -> Void)? = nil) {}
 }
