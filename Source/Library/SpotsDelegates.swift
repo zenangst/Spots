a0369ccb91a935a1480e553ceef178bd7000dfab
Vadym Markov <markov.vadym@gmail.com>
2015-12-10 16:47:15 +0100

    Add spots refresh delegate

--- Source/Library/SpotsDelegates.swift
+++ Source/Library/SpotsDelegates.swift
@@ -5,8 +5,11 @@ public protocol SpotsDelegate: class {
   func spotDidSelectItem(spot: Spotable, item: ListItem)
 }
 
+public protocol SpotsRefreshDelegate: class {
+  func spotsDidReload(refreshControl: UIRefreshControl, completion: (() -> Void)?)
+}
+
 public protocol SpotsScrollDelegate: class {
 
-  func spotsDidReload(refreshControl: UIRefreshControl, completion: (() -> Void)?)
   func spotDidReachEnd(completion: (() -> Void)?)
 }
