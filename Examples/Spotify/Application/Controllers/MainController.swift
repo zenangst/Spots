a647e357d8d15ebd2bb573cf24732f6d5d115b8d
Vadym Markov <markov.vadym@gmail.com>
2015-12-18 16:58:43 +0100

    Convert to tab bar

--- Examples/Spotify/Application/Controllers/MainController.swift
+++ Examples/Spotify/Application/Controllers/MainController.swift
@@ -2,16 +2,44 @@ import Spots
 import Sugar
 import Compass
 
-class MainController: UINavigationController {
+class MainController: UITabBarController {
 
-  lazy var featuredController = FeaturedController(title: "Featured music".uppercaseString)
   lazy var player = PlayerView(frame: UIScreen.mainScreen().bounds)
 
+  lazy var myMusicController: UINavigationController = {
+    let controller = PlaylistController(playlistID: nil)
+    let navigationController = UINavigationController(rootViewController: controller)
+    controller.title = localizedString("My Music")
+
+    return navigationController
+    }()
+
+  lazy var featuredController: UINavigationController = {
+    let controller = FeaturedController(title: localizedString("Featured"))
+    let navigationController = UINavigationController(rootViewController: controller)
+    //featuredController.container.contentInset.bottom = 44
+
+    return navigationController
+    }()
+
   override func viewDidLoad() {
-    viewControllers = [featuredController]
-    featuredController.container.contentInset.bottom = 44
+    super.viewDidLoad()
+    setupTabBar()
 
     player.frame.origin.y = UIScreen.mainScreen().bounds.height - 60
-    view.addSubview(player)
+    //view.addSubview(player)
+  }
+
+  func setupTabBar() {
+    viewControllers = [
+      featuredController,
+      myMusicController
+    ]
+
+    tabBar.translucent = false
+    tabBar.backgroundColor = .blackColor()
+    tabBar.tintColor = UIColor.blackColor()
+
+    selectedIndex = 0
   }
 }
