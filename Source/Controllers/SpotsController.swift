cde0acd9155dd0278d098af4d170d0fac534f0e8
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-21 14:00:03 +0100

    Remove `closure` label

--- Source/Controllers/SpotsController.swift
+++ Source/Controllers/SpotsController.swift
@@ -153,7 +153,7 @@ extension SpotsController {
     }
   }
 
-  public func update(spotAtIndex index: Int = 0, closure: (spot: Spotable) -> Spotable) {
+  public func update(spotAtIndex index: Int = 0, _ closure: (spot: Spotable) -> Spotable) {
     guard let spot = spot(index) else { return }
     spots[spot.index] = closure(spot: spot)
     spot.prepare()
