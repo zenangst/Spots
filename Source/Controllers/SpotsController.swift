a96eb1a24cf76758656e8d5d2c842c87fd81dd57
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-21 09:05:27 +0100

    Add label

--- Source/Controllers/SpotsController.swift
+++ Source/Controllers/SpotsController.swift
@@ -157,7 +157,7 @@ extension SpotsController {
     }
   }
 
-  public func update(index: Int = 0, closure: (spot: Spotable) -> Spotable, completion: (() -> Void)? = nil) {
+  public func update(spotAtIndex index: Int = 0, closure: (spot: Spotable) -> Spotable, completion: (() -> Void)? = nil) {
     guard let spot = spot(index) else { return }
     spots[spot.index] = closure(spot: spot)
     spot.prepare()
