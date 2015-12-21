dfe039c2e7f0fe066116266644935e7be098bd52
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-21 08:42:06 +0100

    Add computed value for getting the first spot

--- Source/Controllers/SpotsController.swift
+++ Source/Controllers/SpotsController.swift
@@ -18,6 +18,12 @@ public class SpotsController: UIViewController, UIScrollViewDelegate {
     didSet { spots.forEach { $0.spotsDelegate = spotsDelegate } }
   }
 
+  var spot: Spotable {
+    get {
+      return spot(0)!
+    }
+  }
+
   weak public var spotsRefreshDelegate: SpotsRefreshDelegate? {
     didSet {
       if spotsRefreshDelegate != nil {
