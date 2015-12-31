47b1204699ecafee584139f6e4ff694a6a2d6934
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-31 19:14:42 +0100

    Rename PlaylistSpotCell

--- Examples/Spotify/Application/Cells/PlaylistListSpotCell.swift
+++ Examples/Spotify/Application/Cells/PlaylistListSpotCell.swift
@@ -1,7 +1,7 @@
 import Spots
 import Imaginary
 
-public class PlaylistSpotCell: UITableViewCell, Itemble {
+public class PlaylistListSpotCell: UITableViewCell, Itemble {
 
   public var size = CGSize(width: 0, height: 60)
   public var item: ListItem?
