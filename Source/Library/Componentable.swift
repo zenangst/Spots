f31db944cd68bdf15d8b560e74a118446ab5d349
Christoffer Winterkvist <christoffer@hyper.no>
2016-01-11 13:57:44 +0100

    Remove files

--- Source/Library/Componentable.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-public protocol Componentable {
-  var height: CGFloat { get }
-  func configure(component: Component)
-}
--- Source/Library/Itemble.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-public protocol Itemble: class {
-  var size: CGSize { get set }
-
-  func configure(inout item: ListItem)
-}
