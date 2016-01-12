4d0d89aa8d564f047e58653246f755d32a60fbe3
Christoffer Winterkvist <christoffer@hyper.no>
2016-01-12 21:35:09 +0100

    Implement views on Viewable

--- Sources/iOS/Library/Viewable.swift
+++ Sources/iOS/Library/Viewable.swift
@@ -47,36 +47,93 @@ public extension Spotable where Self : Viewable {
   }
 
   func append(item: ListItem, completion: (() -> Void)? = nil) {
+    let dynamic = self.dynamicType
+
+    guard dynamic.views.keys.contains(item.kind) else { return }
+
+    let viewClass = dynamic.views[item.kind] ?? dynamic.defaultView
+    let view = viewClass.init().then {
+      ($0 as? Itemble)?.configure(&component.items[index])
+      guard let size = ($0 as? Itemble)?.size else { return }
+      $0.frame.size = size
+    }
+    scrollView.addSubview(view)
     component.items.append(item)
   }
 
   func append(items: [ListItem], completion: (() -> Void)? = nil) {
-    component.items.appendContentsOf(items)
+    for item in items {
+      let dynamic = self.dynamicType
+
+      guard dynamic.views.keys.contains(item.kind) else { return }
+
+      let viewClass = dynamic.views[item.kind] ?? dynamic.defaultView
+      let view = viewClass.init().then {
+        ($0 as? Itemble)?.configure(&component.items[index])
+        guard let size = ($0 as? Itemble)?.size else { return }
+        $0.frame.size = size
+      }
+      scrollView.addSubview(view)
+      component.items.append(item)
+    }
   }
 
   func prepend(items: [ListItem], completion: (() -> Void)? = nil) {
     component.items.insertContentsOf(items, at: 0)
+
+    for item in items.reverse() {
+      let dynamic = self.dynamicType
+
+      guard dynamic.views.keys.contains(item.kind) else { return }
+
+      let viewClass = dynamic.views[item.kind] ?? dynamic.defaultView
+      let view = viewClass.init().then {
+        ($0 as? Itemble)?.configure(&component.items[index])
+        guard let size = ($0 as? Itemble)?.size else { return }
+        $0.frame.size = size
+      }
+      scrollView.insertSubview(view, atIndex: 0)
+      component.items.insert(item, atIndex: 0)
+    }
   }
 
   func insert(item: ListItem, index: Int, completion: (() -> Void)? = nil) {
+    let dynamic = self.dynamicType
+
+    guard dynamic.views.keys.contains(item.kind) else { return }
+
+    let viewClass = dynamic.views[item.kind] ?? dynamic.defaultView
+    let view = viewClass.init().then {
+      ($0 as? Itemble)?.configure(&component.items[index])
+      guard let size = ($0 as? Itemble)?.size else { return }
+      $0.frame.size = size
+    }
+    scrollView.insertSubview(view, atIndex: index)
     component.items.insert(item, atIndex: index)
   }
 
   func update(item: ListItem, index: Int, completion: (() -> Void)? = nil) {
+    guard let view = scrollView.subviews[index] as? Itemble else { return }
+
     component.items[index] = item
+    view.configure(&component.items[index])
   }
 
   func delete(index: Int, completion: (() -> Void)? = nil) {
+    guard index >= 0 && index <= scrollView.subviews.count else { return }
+
     component.items.removeAtIndex(index)
+    scrollView.subviews[index].removeFromSuperview()
   }
 
   func delete(indexes: [Int], completion: (() -> Void)?) {
     for (index, _) in component.items.enumerate() {
+      guard index >= 0 && index <= scrollView.subviews.count else { return }
+
       component.items.removeAtIndex(index)
+      scrollView.subviews[index].removeFromSuperview()
     }
   }
 
-  func reload(indexes: [Int], completion: (() -> Void)? = nil) {
-
-  }
+  func reload(indexes: [Int], completion: (() -> Void)? = nil) { }
 }
