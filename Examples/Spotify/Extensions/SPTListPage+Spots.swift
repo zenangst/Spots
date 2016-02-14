1695582dc343de07690fd01c5e4e504fb4d4a1e2
Christoffer Winterkvist <christoffer@hyper.no>
2016-02-14 18:49:57 +0100

    Improve safety when resolving images

--- Examples/Spotify/Extensions/SPTListPage+Spots.swift
+++ Examples/Spotify/Extensions/SPTListPage+Spots.swift
@@ -12,17 +12,21 @@ extension SPTListPage {
             album = item.album
             else { continue }
 
+          let image = album.largestCover != nil
+          ? album.largestCover.imageURL.absoluteString
+          : ""
+
           viewModels.append(ViewModel(
             title: item.name,
             subtitle:  "\(artist.name) - \(album.name)",
-            image: album.largestCover.imageURL.absoluteString,
+            image: image,
             kind: "playlist",
             action: "play:\(playlistID):\(index + offset)",
             meta: [
               "notification" : "\(item.name) by \(artist.name)",
               "track" : item.name,
               "artist" : artist.name,
-              "image" : album.largestCover.imageURL.absoluteString
+              "image" : image
             ]
             ))
         } else {
