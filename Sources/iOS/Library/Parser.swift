d4d7440c4104ba74772e06c11a56d102493ab103
Christoffer Winterkvist <christoffer@hyper.no>
2016-03-20 19:16:12 +0100

    Clean up parse method

--- Sources/iOS/Library/Parser.swift
+++ Sources/iOS/Library/Parser.swift
@@ -5,10 +5,9 @@ public struct Parser {
 
   public static func parse(json: JSONDictionary) -> [Spotable] {
     guard let components = json["components"] as? JSONArray else { return [] }
-    var spots = [Spotable]()
 
-    components.forEach { spots.append(SpotFactory.resolve(Component($0)))  }
-
-    return spots
+    return components.map {
+      SpotFactory.resolve(Component($0))
+    }
   }
 }
