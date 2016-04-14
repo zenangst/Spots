39c680a6a4c332d6326a41491a2a3e58b3296625
Christoffer Winterkvist <christoffer@hyper.no>
2016-04-14 15:53:35 +0200

    Fix project structure

--- Sources/iOS/Library/ViewRegistry.swift
+++ Sources/iOS/Library/ViewRegistry.swift
@@ -3,7 +3,7 @@ import Brick
 
 public struct ViewRegistry {
   var storage = [String : UIView.Type]()
-  
+
   public subscript(key: StringConvertible) -> UIView.Type? {
     get {
       return storage[key.string]
