c248bed3659271cdd6bf9b11c9d16f25bb505a1b
Christoffer Winterkvist <christoffer@hyper.no>
2016-04-24 22:16:14 +0200

    Remove and cleanup ViewController

--- Examples/Dashboard/Dashboard/ViewController.swift
+++ Examples/Dashboard/Dashboard/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Dashboard
-//
-//  Created by Christoffer Winterkvist on 23/04/16.
-//  Copyright © 2016 Hyper. All rights reserved.
-//
-
 import UIKit
 import Spots
 import Brick
@@ -13,58 +5,34 @@ import Brick
 class ViewController: SpotsController {
 
   convenience init(title: String) {
-    let carouselItems = Component(items: [
-      ViewModel(title: "UX", kind: Cell.Featured, meta: ["color" : UIColor.blackColor()]),
-      ViewModel(title: "Persistency", kind: Cell.Featured, meta: ["color" : UIColor.grayColor()]),
-      ViewModel(title: "Networking", kind: Cell.Featured, meta: ["color" : UIColor.greenColor()]),
-      ViewModel(title: "Navigation", kind: Cell.Featured, meta: ["color" : UIColor.redColor()])
+    let featured = Component(items: [
+      ViewModel(title: "Whisper", image: "https://github.com/hyperoslo/Whisper/raw/master/Resources/whisper-cover.png", kind: Cell.Featured),
+      ViewModel(title: "Spots", image: "https://raw.githubusercontent.com/hyperoslo/Spots/master/Images/cover_v2.png", kind: Cell.Featured),
+      ViewModel(title: "Sync", image: "https://raw.githubusercontent.com/hyperoslo/Sync/master/Images/logo-v2.png", kind: Cell.Featured),
       ])
 
-    let listItems = Component(title: "List Spot", items: [
-      ViewModel(title: "Vadym Markov", subtitle: "iOS Developer", action: "1"),
-      ViewModel(title: "Ramon Gilabert Llop", subtitle: "iOS Developer", action: "2"),
-      ViewModel(title: "Khoa Pham", subtitle: "iOS Developer", action: "3"),
-      ViewModel(title: "Christoffer Winterkvist", subtitle: "iOS Developer", action: "4")
+    let featuredOpensource = Component(span: 3, items: [
+      ViewModel(title: "ImagePicker", image: "https://github.com/hyperoslo/ImagePicker/raw/master/Resources/ImagePickerPresentation.png", kind: Cell.Grid),
+      ViewModel(title: "Spots", image: "https://raw.githubusercontent.com/hyperoslo/Spots/master/Images/cover_v2.png", kind: Cell.Grid),
+      ViewModel(title: "Cache", image: "https://github.com/hyperoslo/Cache/raw/master/Resources/CachePresentation.png", kind: Cell.Grid),
+      ])
+
+    let developers = Component(title: "Swift Developers @hyperoslo", items: [
+      ViewModel(title: "Vadym Markov", image: "https://avatars2.githubusercontent.com/u/10529867?v=3&s=460", subtitle: "iOS Developer", action: "1"),
+      ViewModel(title: "Ramon Gilabert Llop", image: "https://avatars1.githubusercontent.com/u/6138120?v=3&s=460", subtitle: "iOS Developer", action: "2"),
+      ViewModel(title: "Khoa Pham", image: "https://avatars0.githubusercontent.com/u/2284279?v=3&s=460", subtitle: "iOS Developer", action: "3"),
+      ViewModel(title: "Christoffer Winterkvist", image: "https://avatars2.githubusercontent.com/u/57446?v=3&s=460", subtitle: "iOS Developer", action: "4")
       ], meta: ["headerHeight" : 44])
 
-    let featuredOpensource = Component(span: 4, items: [
-      ViewModel(title: "Whisper", kind: Cell.Featured, meta: ["color" : UIColor.blueColor()]),
-      ViewModel(title: "Sync", kind: Cell.Featured, meta: ["color" : UIColor.orangeColor()]),
-      ViewModel(title: "Presentation", kind: Cell.Featured, meta: ["color" : UIColor.yellowColor()]),
-      ViewModel(title: "HUE", kind: Cell.Featured, meta: ["color" : UIColor.redColor()]),
-      ])
-
-    let gridItems = Component(span: 6, items: [
-      ViewModel(title: "ImagePicker", kind: Cell.Featured, meta: ["color" : UIColor.darkGrayColor()]),
-      ViewModel(title: "Sugar", kind: Cell.Featured, meta: ["color" : UIColor.redColor()]),
-      ViewModel(title: "Cache", kind: Cell.Featured, meta: ["color" : UIColor.greenColor()]),
-      ViewModel(title: "Spots", kind: Cell.Featured, meta: ["color" : UIColor.blackColor()]),
-      ViewModel(title: "Compass", kind: Cell.Featured, meta: ["color" : UIColor.blueColor()]),
-      ViewModel(title: "Pages", kind: Cell.Featured, meta: ["color" : UIColor.redColor()])
-      ])
-
     let spots: [Spotable] = [
-      ListSpot(component: Component(title: "Carousel Spot", meta: ["headerHeight" : 44])),
-      CarouselSpot(carouselItems, top: 5, left: 0, bottom: 5, right: 0, itemSpacing: 0),
-      ListSpot(component: Component(title: "Grid Spot", meta: ["headerHeight" : 44])),
-      GridSpot(featuredOpensource, top: 10, left: 10, bottom: 20, right: 10, itemSpacing: -5),
-      ListSpot(component: listItems),
-      ListSpot(component: Component(title: "Grid Spot", meta: ["headerHeight" : 44])),
-      GridSpot(gridItems, top: 10, left: 10, bottom: 20, right: 10, itemSpacing: -5),
+      ListSpot(component: Component(title: "Spots for tvOS", meta: ["headerHeight" : 44])),
+      CarouselSpot(featured, top: 0, left: 0, bottom: 30, right: 0, itemSpacing: 0),
+      ListSpot(component: Component(title: "Open source by @hyperoslo", meta: ["headerHeight" : 44])),
+      GridSpot(featuredOpensource, top: 30, left: 30, bottom: 30, right: 30, itemSpacing: -5),
+      ListSpot(component: developers),
       ]
 
     self.init(spots: spots)
   }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
