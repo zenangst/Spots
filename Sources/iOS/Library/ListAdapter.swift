0cc6d8fb77d13565a11eef7c7f920c9b500ae821
Vadym Markov <markov.vadym@gmail.com>
2016-05-09 23:42:27 +0200

    Don't show header if header height is 0.0

--- Sources/iOS/Library/ListAdapter.swift
+++ Sources/iOS/Library/ListAdapter.swift
@@ -63,6 +63,8 @@ extension ListAdapter: UITableViewDelegate {
    - Returns: A view object to be displayed in the header of section based on the kind of the ListSpot and registered headers.
  */
   public func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    guard spot.component.meta("headerHeight", type: CGFloat.self) != 0.0 else { return nil}
+
     let reuseIdentifer = spot.component.kind.isPresent ? spot.component.kind : spot.dynamicType.defaultKind
 
     if let listSpot = spot as? ListSpot, cachedHeader = listSpot.cachedHeaders[reuseIdentifer.string] {
