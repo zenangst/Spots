a6d45414c4122ed409d5166a55462e1252eecded
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-09 15:33:18 +0200

    Add reloadIfNeeded with JSON

--- Sources/iOS/Controllers/SpotsController.swift
+++ Sources/iOS/Controllers/SpotsController.swift
@@ -219,6 +219,32 @@ extension SpotsController {
    - Parameter json: A JSON dictionary that gets parsed into UI elements
    - Parameter completion: A closure that will be run after reload has been performed on all spots
   */
+  public func reloadIfNeeded(json: [String : AnyObject], animated: ((view: UIView) -> Void)? = nil, closure: (() -> Void)? = nil) {
+    let newSpots = Parser.parse(json)
+    
+    let newComponents = newSpots.map { $0.component }
+    let oldComponents = spots.map { $0.component }
+
+    guard oldComponents != newComponents else { cache(); return }
+    
+    spots = newSpots
+    cache()
+
+    if spotsScrollView.superview == nil {
+      view.addSubview(spotsScrollView)
+    }
+
+    spotsScrollView.contentView.subviews.forEach { $0.removeFromSuperview() }
+    setupSpots(animated)
+    spotsScrollView.forceUpdate = true
+
+    closure?()
+  }
+
+  /**
+   - Parameter json: A JSON dictionary that gets parsed into UI elements
+   - Parameter completion: A closure that will be run after reload has been performed on all spots
+  */
   public func reload(json: [String : AnyObject], animated: ((view: UIView) -> Void)? = nil, closure: (() -> Void)? = nil) {
     spots = Parser.parse(json)
     cache()
