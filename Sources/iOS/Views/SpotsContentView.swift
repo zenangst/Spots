b5442c3aeb2d1f4b8be83b1445e2e447b23fd701
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-12 12:16:50 +0200

    Add documentation to SpotsContentView.swift

--- Sources/iOS/Views/SpotsContentView.swift
+++ Sources/iOS/Views/SpotsContentView.swift
@@ -2,6 +2,11 @@ import UIKit
 
 public class SpotsContentView : UIView {
 
+  /**
+   Tells the view that a subview was added.
+   
+   - Parameter subview: The view that was added as a subview.
+   */
   override public func didAddSubview(subview: UIView) {
     super.didAddSubview(subview)
 
@@ -9,6 +14,11 @@ public class SpotsContentView : UIView {
     containerScrollView.didAddSubviewToContainer(subview)
   }
 
+  /**
+   Tells the view that a subview is about to be removed.
+   
+   - Parameter subview: Tells the view that a subview is about to be removed.
+   */
   override public func willRemoveSubview(subview: UIView) {
     super.willRemoveSubview(subview)
 
