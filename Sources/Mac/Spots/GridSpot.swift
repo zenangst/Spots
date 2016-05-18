271a562e90eeb9029a3bb67b9c82ee906159bebb
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-18 23:43:06 +0200

    Refactor GridSpot

--- Sources/Mac/Spots/GridSpot.swift
+++ Sources/Mac/Spots/GridSpot.swift
@@ -2,10 +2,12 @@ import Cocoa
 import Sugar
 import Brick
 
-public class GridSpot: NSObject, Spotable {
+public class GridSpot: NSObject, Gridable {
 
   public static var views = ViewRegistry()
-  public static var defaultView: RegularView.Type = GridSpotCell.self
+  public static var grids = GridRegistry()
+  public static var configure: ((view: NSCollectionView) -> Void)?
+  public static var defaultView: RegularView.Type = NSView.self
   public static var defaultKind: StringConvertible = "grid"
 
   public weak var spotsDelegate: SpotsDelegate?
@@ -17,16 +19,25 @@ public class GridSpot: NSObject, Spotable {
 
   public private(set) var stateCache: SpotCache?
   
-  public lazy var collectionView: NSCollectionView = NSCollectionView(frame: CGRectZero).then {_ in
-//    $0.backgroundColor = UIColor.whiteColor()
-//    $0.dataSource = self.adapter
-//    $0.delegate = self.adapter
-//    $0.scrollEnabled = false
+  public lazy var adapter: CollectionAdapter = CollectionAdapter(spot: self)
+  @available(OSX 10.11, *)
+  public lazy var layout = NSCollectionViewFlowLayout()
+
+  public lazy var scrollView: ScrollView = ScrollView().then {
+    $0.documentView = NSView()
+    $0.autoresizingMask = [.ViewWidthSizable]
+  }
+
+  public lazy var collectionView: NSCollectionView = NSCollectionView().then {
+    $0.autoresizingMask = [.ViewWidthSizable]
   }
 
   public required init(component: Component) {
     self.component = component
     super.init()
+    
+    setupCollectionView()
+    scrollView.contentView.addSubview(collectionView)
   }
 
   public convenience init(title: String = "", kind: String? = nil) {
@@ -42,11 +53,29 @@ public class GridSpot: NSObject, Spotable {
     //prepare()
   }
 
-  public func render() -> RegularView {
-    return collectionView
+  public func setupCollectionView() {
+//    collectionView.maxNumberOfColumns = Int(component.span)
+    if #available(OSX 10.11, *) {
+      collectionView.delegate = adapter
+      collectionView.dataSource = adapter
+      collectionView.collectionViewLayout = layout
+    } else {
+      // Fallback on earlier versions
+    }
   }
 
-  public func setup(size: CGSize) {}
+  public func render() -> RegularView {
+    return scrollView
+  }
+
+  public func setup(size: CGSize) {
+    component.items.enumerate().forEach {
+      component.items[$0.index].size.width = size.width
+    }
+    scrollView.frame.size = size
+    GridSpot.configure?(view: collectionView)
+  }
+  
   public func append(item: ViewModel, withAnimation animation: SpotsAnimation, completion: Completion) {}
   public func append(items: [ViewModel], withAnimation animation: SpotsAnimation, completion: Completion) {}
   public func prepend(items: [ViewModel], withAnimation animation: SpotsAnimation, completion: Completion) {}
