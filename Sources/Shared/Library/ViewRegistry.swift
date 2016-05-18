8ab0e7a8578052a57809dc6c1ae90be3c295e98a
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-18 21:27:33 +0200

    Move ViewRegistry to Shared

--- Sources/Shared/Library/ViewRegistry.swift
+++ Sources/Shared/Library/ViewRegistry.swift
@@ -1,17 +1,16 @@
-import UIKit
 import Brick
 
 /// A view registry that is used internally when resolving kind to the corresponding spot.
 public struct ViewRegistry {
   /// A Key-value dictionary of registred types
-  var storage = [String : UIView.Type]()
+  var storage = [String : RegularView.Type]()
 
   /**
    A subscripting method for getting a value from storage using a StringConvertible key
 
    - Returns: An optional UIView type
- */
-  public subscript(key: StringConvertible) -> UIView.Type? {
+   */
+  public subscript(key: StringConvertible) -> RegularView.Type? {
     get {
       return storage[key.string]
     }
