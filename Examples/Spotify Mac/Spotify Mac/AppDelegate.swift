0dc8d6df03a7e03eae90dfd40f842273ee8e233f
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-19 00:43:31 +0200

    Refactor AppDelegate

--- Examples/Spotify Mac/Spotify Mac/AppDelegate.swift
+++ Examples/Spotify Mac/Spotify Mac/AppDelegate.swift
@@ -11,17 +11,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(aNotification: NSNotification) {
 
-    ListSpot.views["list"] = TableViewCell.self
+    ListSpot.views["list"] = ListSpotItem.self
     GridSpot.grids["grid"] = GridSpotItem.self
 
+//    ["kind" : "grid", "items" : [["title" : "hello", "kind" : "grid", "size" : ["height" : 88]]]],
+
+    let items: [[String : AnyObject]] = [
+      ["title" : "foo", "subtitle" : "bar", "kind" : "list", "size" : ["height" : 44]],
+      ["title" : "foo", "subtitle" : "bar", "kind" : "list", "size" : ["height" : 44]],
+      ["title" : "foo", "kind" : "list", "size" : ["height" : 44]]
+    ]
     let spotsController = SpotsController([
       "components" : [
-        ["kind" : "grid", "items" : [["title" : "hello", "kind" : "grid", "size" : ["height" : 88]]]],
-        ["kind" : "list", "items" : [["title" : "hello", "kind" : "list", "size" : ["height" : 44]]]]
+        ["kind" : "list", "items" : items]
       ]
       ])
 
     window.contentView = spotsController.view
+    window.becomeKeyWindow()
   }
 
   func applicationWillTerminate(aNotification: NSNotification) {
