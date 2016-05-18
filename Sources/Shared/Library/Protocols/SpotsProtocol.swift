0e57c23a990288885ff746bebfa10885e826e8f0
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-18 21:23:50 +0200

    Add shared SpotsProtocol to share across platforms

---
+++ Sources/Shared/Library/Protocols/SpotsProtocol.swift
@@ -0,0 +1,249 @@
+#if os(iOS)
+  import UIKit
+#else
+  import Foundation
+#endif
+
+import Sugar
+import Brick
+
+public protocol SpotsProtocol: class {
+  var stateCache: SpotCache? { get }
+  var spotsScrollView: SpotsScrollView { get }
+  var spotsDelegate: SpotsDelegate? { get }
+  var spots: [Spotable] { get set }
+  var spot: Spotable? { get }
+  var refreshPositions: [CGFloat] { get }
+}
+
+public extension SpotsProtocol {
+
+  public var dictionary: JSONDictionary {
+    get {
+      return ["components" : spots.map { $0.component.dictionary }]
+    }
+  }
+
+  /**
+   A generic look up method for resolving spots based on index
+
+   - Parameter index: The index of the spot that you are trying to resolve
+   - Parameter type: The generic type for the spot you are trying to resolve
+   */
+  public func spot<T>(index: Int = 0, _ type: T.Type) -> T? {
+    return spots.filter({ $0.index == index }).first as? T
+  }
+
+  /**
+   A generic look up method for resolving spots using a closure
+
+   - Parameter closure: A closure to perform actions on a spotable object
+   */
+  public func spot(@noescape closure: (index: Int, spot: Spotable) -> Bool) -> Spotable? {
+    for (index, spot) in spots.enumerate()
+      where closure(index: index, spot: spot) {
+        return spot
+    }
+    return nil
+  }
+
+  /**
+   - Parameter includeElement: A filter predicate to find a spot
+   */
+  public func filter(@noescape includeElement: (Spotable) -> Bool) -> [Spotable] {
+    return spots.filter(includeElement)
+  }
+
+  /**
+   - Parameter completion: A closure that will be run after reload has been performed on all spots
+   */
+  public func reload(animated: Bool = true, withAnimation animation: SpotsAnimation = .Automatic, completion: Completion = nil) {
+    var spotsLeft = spots.count
+
+    dispatch { [weak self] in
+      self?.spots.forEach { spot in
+        spot.reload([], withAnimation: animation) {
+          spotsLeft -= 1
+
+          if spotsLeft == 0 {
+            completion?()
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   - Parameter spotAtIndex: The index of the spot that you want to perform updates on
+   - Parameter animated: Perform reload animation
+   - Parameter closure: A transform closure to perform the proper modification to the target spot before updating the internals
+   */
+  public func update(spotAtIndex index: Int = 0, withAnimation animation: SpotsAnimation = .Automatic, @noescape _ closure: (spot: Spotable) -> Void) {
+    guard let spot = spot(index, Spotable.self) else { return }
+    closure(spot: spot)
+    spot.refreshIndexes()
+    #if os(iOS)
+    spot.prepare()
+      #endif
+
+    dispatch { [weak self] in
+      guard let weakSelf = self else { return }
+
+      weakSelf.spot(spot.index, Spotable.self)?.reload([index], withAnimation: animation) {
+      #if os(iOS)
+        weakSelf.spotsScrollView.setNeedsDisplay()
+      #endif
+        weakSelf.spotsScrollView.forceUpdate = true
+      }
+    }
+  }
+
+  /**
+   Updates spot only if the passed view models are not the same with the current ones.
+   - Parameter spotAtIndex: The index of the spot that you want to perform updates on
+   - Parameter items: An array of view models
+   */
+  public func updateIfNeeded(spotAtIndex index: Int = 0, items: [ViewModel], withAnimation animation: SpotsAnimation = .Automatic) {
+    guard let spot = spot(index, Spotable.self) where !(spot.items == items) else { return }
+
+    update(spotAtIndex: index, withAnimation: animation) {
+      $0.items = items
+    }
+  }
+
+  /**
+   - Parameter item: The view model that you want to append
+   - Parameter spotIndex: The index of the spot that you want to append to, defaults to 0
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func append(item: ViewModel, spotIndex: Int = 0, withAnimation animation: SpotsAnimation = .None, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.append(item, withAnimation: animation) {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+
+  /**
+   - Parameter items: A collection of view models
+   - Parameter spotIndex: The index of the spot that you want to append to, defaults to 0
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func append(items: [ViewModel], spotIndex: Int = 0, withAnimation animation: SpotsAnimation = .None, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.append(items, withAnimation: animation) {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+
+  /**
+   - Parameter items: A collection of view models
+   - Parameter spotIndex: The index of the spot that you want to prepend to, defaults to 0
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func prepend(items: [ViewModel], spotIndex: Int = 0, withAnimation animation: SpotsAnimation = .None, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.prepend(items, withAnimation: animation)  {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+
+  /**
+   - Parameter item: The view model that you want to insert
+   - Parameter index: The index that you want to insert the view model at
+   - Parameter spotIndex: The index of the spot that you want to insert into
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func insert(item: ViewModel, index: Int = 0, spotIndex: Int, withAnimation animation: SpotsAnimation = .None, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.insert(item, index: index, withAnimation: animation)  {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+
+  /**
+   - Parameter item: The view model that you want to update
+   - Parameter index: The index that you want to insert the view model at
+   - Parameter spotIndex: The index of the spot that you want to update into
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func update(item: ViewModel, index: Int = 0, spotIndex: Int, withAnimation animation: SpotsAnimation = .None, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.update(item, index: index, withAnimation: animation)  {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+
+  /**
+   - Parameter indexes: An integer array of indexes that you want to update
+   - Parameter spotIndex: The index of the spot that you want to update into
+   - Parameter animated: Perform reload animation
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func update(indexes indexes: [Int], spotIndex: Int = 0, withAnimation animation: SpotsAnimation = .Automatic, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.reload(indexes, withAnimation: animation) {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+
+  /**
+   - Parameter index: The index of the view model that you want to remove
+   - Parameter spotIndex: The index of the spot that you want to remove into
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func delete(index: Int, spotIndex: Int = 0, withAnimation animation: SpotsAnimation = .None, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.delete(index, withAnimation: animation) {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+
+  /**
+   - Parameter indexes: A collection of indexes for view models that you want to remove
+   - Parameter spotIndex: The index of the spot that you want to remove into
+   - Parameter closure: A completion closure that will run after the spot has performed updates internally
+   */
+  public func delete(indexes indexes: [Int], spotIndex: Int = 0, withAnimation animation: SpotsAnimation = .None, completion: Completion = nil) {
+    spot(spotIndex, Spotable.self)?.delete(indexes, withAnimation: animation) {
+      completion?()
+      self.spotsScrollView.forceUpdate = true
+    }
+    spot(spotIndex, Spotable.self)?.refreshIndexes()
+  }
+#if os(iOS)
+  /**
+   - Parameter index: The index of the spot that you want to scroll
+   - Parameter includeElement: A filter predicate to find a view model
+   */
+  public func scrollTo(spotIndex index: Int = 0, @noescape includeElement: (ViewModel) -> Bool) {
+    guard let itemY = spot(index, Spotable.self)?.scrollTo(includeElement) else { return }
+
+    if spot(index, Spotable.self)?.spotHeight() > spotsScrollView.height - spotsScrollView.contentInset.bottom {
+      let y = itemY - spotsScrollView.height + spotsScrollView.contentInset.bottom
+      spotsScrollView.setContentOffset(CGPoint(x: CGFloat(0.0), y: y), animated: true)
+    }
+  }
+
+  /**
+   - Parameter animated: A boolean value to determine if you want to perform the scrolling with or without animation
+   */
+  public func scrollToBottom(animated: Bool) {
+    let y = spotsScrollView.contentSize.height - spotsScrollView.height + spotsScrollView.contentInset.bottom
+    spotsScrollView.setContentOffset(CGPoint(x: 0, y: y), animated: animated)
+  }
+#endif
+
+  /**
+   Caches the current state of the spot controller
+   */
+  public func cache() {
+    stateCache?.save(dictionary)
+  }
+}
