d3b7ed46056900a3201cda1703c1d5ef242b8432
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-18 23:44:08 +0200

    Add GridSpotItem

---
+++ Sources/Mac/Views/GridSpotItem.swift
@@ -0,0 +1,24 @@
+import Cocoa
+import Spots
+import Brick
+
+public class GridSpotItem: NSCollectionViewItem, SpotConfigurable {
+
+  public var size = CGSize(width: 0, height: 88)
+  public var customView = NSView()
+
+  public override func loadView() {
+    view = customView
+  }
+
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+    view.wantsLayer = true
+    view.layer?.backgroundColor = NSColor.orangeColor().CGColor
+  }
+
+  public func configure(inout item: ViewModel) {
+    title = item.title
+    NSLog("item : \(item.title)")
+  }
+}
