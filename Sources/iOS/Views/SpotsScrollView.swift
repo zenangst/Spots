fcc2503f4b9ba4be35ca0dd208a3cf2516170540
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-05-19 15:11:53 +0200

    Remove faulty if-statement
    I cannot remember what this should fix but it just causes issues so, I'll just remove it.

--- Sources/iOS/Views/SpotsScrollView.swift
+++ Sources/iOS/Views/SpotsScrollView.swift
@@ -127,9 +127,7 @@ public class SpotsScrollView: UIScrollView {
         if subview is UITableView {
           let remainingBoundsHeight = fmax(CGRectGetMaxY(bounds) - CGRectGetMinY(frame), 0.0)
           let remainingContentHeight = fmax(scrollView.contentSize.height - contentOffset.y, 0.0)
-          if subviewsInLayoutOrder.count > 1 {
-            frame.size.height = ceil(fmin(remainingBoundsHeight, remainingContentHeight))
-          }
+          frame.size.height = ceil(fmin(remainingBoundsHeight, remainingContentHeight))
         }
 
         frame.size.width = ceil(contentView.frame.size.width)
