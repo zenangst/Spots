d040456ec429398e60d2d06ac2210e67297ee31d
Christoffer Winterkvist <christoffer@hyper.no>
2016-06-08 11:25:35 +0200

    Fix dispatch updates

--- Sources/iOS/Controllers/SpotsController.swift
+++ Sources/iOS/Controllers/SpotsController.swift
@@ -247,12 +247,12 @@ extension SpotsController {
    - Parameter completion: A closure that will be run after reload has been performed on all spots
   */
   public func reloadIfNeeded(json: [String : AnyObject], animated: ((view: UIView) -> Void)? = nil, closure: Completion = nil) {
-    let newSpots = Parser.parse(json)
-    let newComponents = newSpots.map { $0.component }
-    let oldComponents = spots.map { $0.component }
-
     dispatch { [weak self] in
       guard let weakSelf = self else { closure?(); return }
+
+      let newSpots = Parser.parse(json)
+      let newComponents = newSpots.map { $0.component }
+      let oldComponents = weakSelf.spots.map { $0.component }
 
       guard oldComponents != newComponents else {
         weakSelf.cache()
