91ecf73b33e218c5ace7e210d2b40c0938feca06
Christoffer Winterkvist <christoffer@hyper.no>
2016-06-15 10:41:24 +0200

    Remove setting a background color to the CarouselSpot

--- Sources/iOS/Spots/Carousel/CarouselSpot.swift
+++ Sources/iOS/Spots/Carousel/CarouselSpot.swift
@@ -59,7 +59,6 @@ public class CarouselSpot: NSObject, Gridable {
   }
 
   public lazy var collectionView: UICollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: self.layout).then {
-    $0.backgroundColor = UIColor.whiteColor()
     $0.dataSource = self.adapter
     $0.delegate = self.adapter
     $0.showsHorizontalScrollIndicator = false
