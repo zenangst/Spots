a4c1f12566d1f4d0f76f69b3ab0c615fac512d9b
Christoffer Winterkvist <christoffer@hyper.no>
2016-06-23 12:18:58 +0200

    Carthage update

---
+++ Sources/iOS/Extensions/ListAdapter+iOS.swift
@@ -0,0 +1,9 @@
+//
+//  ListAdapter+iOS.swift
+//  Spots
+//
+//  Created by Christoffer Winterkvist on 6/23/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
