64ab3000aaa05251e3a4ca1a8316b4178c1d1bb7
Christoffer Winterkvist <christoffer@hyper.no>
2016-06-30 11:17:50 +0200

    Refactor Window.swift

--- Examples/SpotifyMac/Spotify Mac/Window.swift
+++ Examples/SpotifyMac/Spotify Mac/Window.swift
@@ -30,19 +30,24 @@ class Window: NSWindow {
   override init (contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, defer flag: Bool) {
     super.init (contentRect: contentRect, styleMask: aStyle, backing: bufferingType, defer: flag)
 
-    self.opaque = false
     self.titleVisibility = NSWindowTitleVisibility.Hidden
     self.toolbar = toolbar
     self.styleMask =
       NSClosableWindowMask |
       NSMiniaturizableWindowMask |
       NSResizableWindowMask |
-      NSTexturedBackgroundWindowMask |
       NSBorderlessWindowMask |
       NSTitledWindowMask
     self.opaque = false
     self.titlebarAppearsTransparent = true
-    self.backgroundColor = NSColor(red:0.157, green:0.157, blue:0.157, alpha: 1)
+    self.backgroundColor = NSColor(red:0.165, green:0.169, blue:0.169, alpha: 1)
+    self.movable = true
+    self.movableByWindowBackground = true
+
+    if let contentView = contentView, layer = contentView.layer as? CAGradientLayer,
+    colors = layer.colors as? [CGColorRef], firstColor = colors.first {
+      self.backgroundColor = NSColor(CGColor: firstColor)
+    }
   }
 
   required init?(coder: NSCoder) {
