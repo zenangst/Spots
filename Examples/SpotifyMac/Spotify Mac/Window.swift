5b7d29b83599eeb8ed6971832daad758b4e6675d
Christoffer Winterkvist <christoffer@hyper.no>
2016-07-04 09:32:05 +0200

    Refactor Window class

--- Examples/SpotifyMac/Spotify Mac/Window.swift
+++ Examples/SpotifyMac/Spotify Mac/Window.swift
@@ -1,6 +1,8 @@
 import Cocoa
 
-class Window: NSWindow {
+class Window: NSWindow, NSWindowDelegate {
+
+  lazy var customToolbar = Toolbar(identifier: "main-toolbar")
 
   override var canBecomeMainWindow: Bool {
     get { return true }
@@ -16,10 +18,11 @@ class Window: NSWindow {
 
       let gradientLayer = CAGradientLayer()
       gradientLayer.colors = [
-        NSColor(red:0.165, green:0.169, blue:0.169, alpha: 1).CGColor,
-        NSColor.blackColor().CGColor
+        NSColor(red:0.3, green:0.3, blue:0.3, alpha: 1).CGColor,
+        NSColor(red:0.2, green:0.2, blue:0.2, alpha: 1).CGColor,
+        NSColor(red:0.1, green:0.1, blue:0.1, alpha: 1).CGColor,
       ]
-      gradientLayer.locations = [0.0, 1.0]
+      gradientLayer.locations = [0.0, 0.03, 0.7]
       gradientLayer.frame.size = contentView.frame.size
 
       contentView.wantsLayer = true
@@ -30,27 +33,39 @@ class Window: NSWindow {
   override init (contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, defer flag: Bool) {
     super.init (contentRect: contentRect, styleMask: aStyle, backing: bufferingType, defer: flag)
 
-    self.titleVisibility = NSWindowTitleVisibility.Hidden
-    self.toolbar = toolbar
+    self.titleVisibility = .Hidden
     self.styleMask =
       NSClosableWindowMask |
       NSMiniaturizableWindowMask |
       NSResizableWindowMask |
       NSBorderlessWindowMask |
-      NSTitledWindowMask
+      NSTitledWindowMask |
+      NSFullSizeContentViewWindowMask
     self.opaque = false
     self.titlebarAppearsTransparent = true
-    self.backgroundColor = NSColor(red:0.165, green:0.169, blue:0.169, alpha: 1)
+    self.toolbar = customToolbar
+    self.minSize = NSSize(width: 960, height: 640)
     self.movable = true
-    self.movableByWindowBackground = true
+    self.delegate = self
 
     if let contentView = contentView, layer = contentView.layer as? CAGradientLayer,
     colors = layer.colors as? [CGColorRef], firstColor = colors.first {
-      self.backgroundColor = NSColor(CGColor: firstColor)
+      self.backgroundColor = NSColor(CGColor: firstColor)?.alpha(1.0)
     }
   }
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension Window {
+
+  func windowDidExitFullScreen(notification: NSNotification) {
+    toolbar = customToolbar
+  }
+
+  func windowWillEnterFullScreen(notification: NSNotification) {
+    toolbar = nil
   }
 }
