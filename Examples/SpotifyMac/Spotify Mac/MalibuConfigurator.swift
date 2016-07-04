58d811d08c92563e21ffff1dc664242516df9ef4
Christoffer Winterkvist <christoffer@hyper.no>
2016-07-04 09:34:01 +0200

    Set Malibu logger to log errors

--- Examples/SpotifyMac/Spotify Mac/MalibuConfigurator.swift
+++ Examples/SpotifyMac/Spotify Mac/MalibuConfigurator.swift
@@ -5,7 +5,7 @@ struct MalibuConfigurator: Configurator {
   func configure() {
     let networking = Networking(baseURLString: "https://api.spotify.com/v1/")
     Malibu.register("api", networking: networking)
-//    Malibu.logger.level = .Error
+    Malibu.logger.level = .Error
 
     Malibu.networking("api").additionalHeaders = {
       ["Accept" : "application/json"]
