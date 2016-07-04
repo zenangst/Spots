6f427786904045e1ff19197af1d9a388af6ed798
Christoffer Winterkvist <christoffer@hyper.no>
2016-07-04 09:35:24 +0200

    Add Blueprints struct

---
+++ Examples/SpotifyMac/Spotify Mac/Blueprints.swift
@@ -0,0 +1,18 @@
+import Spots
+import Sugar
+import Malibu
+import Brick
+
+public struct Blueprints {
+
+  var storage = [String : Blueprint]()
+
+  public subscript(key: StringConvertible) -> Blueprint? {
+    get {
+      return storage[key.string]
+    }
+    set(value) {
+      storage[key.string] = value
+    }
+  }
+}
