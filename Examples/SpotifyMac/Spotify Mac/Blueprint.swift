98c7f8b333e68abafc2231674f2f32f05ceda537
Christoffer Winterkvist <christoffer@hyper.no>
2016-07-04 09:35:30 +0200

    Add Blueprint struct

---
+++ Examples/SpotifyMac/Spotify Mac/Blueprint.swift
@@ -0,0 +1,29 @@
+import Spots
+import Sugar
+import Malibu
+import Brick
+
+public struct Blueprint {
+
+  public var cacheKey: String
+  public var fragmentHandler: ((fragments: [String : AnyObject], controller: SpotsController) -> Void)? = nil
+  public var requests: [(request: GETRequestable?, rootKey: String, spotIndex: Int, adapter: (json: JSONArray) -> [ViewModel])]
+  public var template: JSONDictionary
+
+  init(cacheKey: String,
+       requests: [(request: GETRequestable?, rootKey: String, spotIndex: Int, adapter: (json: JSONArray) -> [ViewModel])] = [],
+       fragmentHandler: ((fragments: [String : AnyObject], controller: SpotsController) -> Void)? = nil,
+       template: JSONDictionary) {
+    self.cacheKey = cacheKey
+    self.fragmentHandler = fragmentHandler
+    self.requests = requests
+    self.template = template
+  }
+}
+
+extension Blueprint {
+
+  mutating func cacheKey(key: String) {
+    cacheKey = key
+  }
+}
