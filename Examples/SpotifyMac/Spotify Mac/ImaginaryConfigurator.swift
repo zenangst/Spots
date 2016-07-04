588eefbe8ebc03af4a34dc1e73045b261a37f8ed
Christoffer Winterkvist <christoffer@hyper.no>
2016-07-04 09:34:06 +0200

    Add newline

--- Examples/SpotifyMac/Spotify Mac/ImaginaryConfigurator.swift
+++ Examples/SpotifyMac/Spotify Mac/ImaginaryConfigurator.swift
@@ -3,6 +3,7 @@ import Imaginary
 struct ImaginaryConfigurator: Configurator {
 
   func configure() {
+    
     Imaginary.Configuration.preConfigure = nil
     Imaginary.Configuration.postConfigure = nil
     Imaginary.Configuration.transitionClosure = { imageView, image in
