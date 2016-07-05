22613a6912c2eec99a0b4fed857990f5ff289f55
Christoffer Winterkvist <christoffer@hyper.no>
2016-07-05 23:48:12 +0200

    Move init methods to controllers

--- Sources/Mac/Controllers/SpotsController.swift
+++ Sources/Mac/Controllers/SpotsController.swift
@@ -61,24 +61,36 @@ public class SpotsController: NSViewController, SpotsProtocol {
   }
 
   /**
-   deinit
-   */
-  deinit {
-    NSNotificationCenter.defaultCenter().removeObserver(self)
-    for spot in spots {
-      spots.forEach { $0.spotsDelegate = nil }
-    }
-    spotsDelegate = nil
-    spotsScrollDelegate = nil
-  }
-
-  /**
    - Parameter cacheKey: A key that will be used to identify the SpotCache
    */
   public convenience init(cacheKey: String) {
     let stateCache = SpotCache(key: cacheKey)
     self.init(spots: Parser.parse(stateCache.load()))
     self.stateCache = stateCache
+  }
+
+  /**
+   - Parameter spot: A Spotable object
+   */
+  public convenience init(spot: Spotable) {
+    self.init(spots: [spot])
+  }
+
+  /**
+   - Parameter json: A JSON dictionary that gets parsed into UI elements
+   */
+  public convenience init(_ json: [String : AnyObject]) {
+    self.init(spots: Parser.parse(json))
+  }
+
+  /**
+   deinit
+   */
+  deinit {
+    NSNotificationCenter.defaultCenter().removeObserver(self)
+    spots.forEach { $0.spotsDelegate = nil }
+    spotsDelegate = nil
+    spotsScrollDelegate = nil
   }
 
   /**
