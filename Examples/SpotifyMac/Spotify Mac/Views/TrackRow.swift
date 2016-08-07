909f9fc37b885d073ac53579a61ff97c6f44c8e0
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-07 20:13:15 +0200

    Refactor TrackRow

--- Examples/SpotifyMac/Spotify Mac/Views/TrackRow.swift
+++ Examples/SpotifyMac/Spotify Mac/Views/TrackRow.swift
@@ -23,24 +23,35 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
     didSet {
       if selected {
         titleLabel.textColor = NSColor.whiteColor()
-        subtitleLabel.textColor = NSColor.lightGrayColor()
+        durationLabel.textColor = NSColor.whiteColor()
+        titleLabel.font = NSFont.boldSystemFontOfSize(12)
+        subtitleLabel.textColor = NSColor.whiteColor()
         if tintColor != nil {
           imageView.tintColor = tintColor
         }
         trackLabel.textColor = NSColor.whiteColor()
 
         let gradientLayer = CAGradientLayer()
+
         gradientLayer.colors = [
           NSColor(red:0.216, green:0.824, blue:0.278, alpha: 0.75).CGColor,
           NSColor(red:0.216, green:0.824, blue:0.278, alpha: 0.325).CGColor,
         ]
+
         layer = gradientLayer
 
         layer?.cornerRadius = 3.0
+
+        if let item = item where !item.image.hasPrefix("http") {
+          imageView.tintColor = NSColor.whiteColor()
+        }
+
         playButton.tintColor = NSColor.whiteColor()
       } else {
-        titleLabel.textColor = NSColor.lightGrayColor()
-        subtitleLabel.textColor = NSColor.darkGrayColor()
+        titleLabel.font = NSFont.systemFontOfSize(12)
+        titleLabel.textColor = NSColor.whiteColor()
+        durationLabel.textColor = NSColor.lightGrayColor()
+        subtitleLabel.textColor = NSColor.lightGrayColor()
         trackLabel.textColor = NSColor.darkGrayColor()
         if tintColor != nil { imageView.tintColor = NSColor.grayColor() }
         layer = CALayer()
@@ -50,7 +61,7 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
     }
   }
 
-  lazy var imageView = NSImageView()
+  lazy var imageView: ClickableImageView = ClickableImageView()
   lazy var playButton = NSImageView()
 
   public lazy var trackLabel = NSTextField().then {
@@ -68,13 +79,21 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
     $0.drawsBackground = false
   }
 
-  public lazy var subtitleLabel = NSTextField().then {
+  public lazy var subtitleLabel: ClickableTextField = ClickableTextField().then {
     $0.editable = false
     $0.selectable = false
     $0.bezeled = false
     $0.drawsBackground = false
     $0.cell?.wraps = true
     $0.cell?.lineBreakMode = .ByWordWrapping
+  }
+
+  public lazy var durationLabel = NSTextField().then {
+    $0.editable = false
+    $0.selectable = false
+    $0.bezeled = false
+    $0.drawsBackground = false
+    $0.font = NSFont.systemFontOfSize(11)
   }
 
   lazy var lineView = NSView().then {
@@ -95,9 +114,18 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
     addSubview(trackLabel)
     addSubview(titleLabel)
     addSubview(subtitleLabel)
+    addSubview(durationLabel)
     addSubview(lineView)
     addSubview(imageView)
     addSubview(playButton)
+
+    imageView.target = self
+    imageView.clickType = .Double
+    imageView.clickAction = #selector(TrackRow.clickAlbum(_:))
+    imageView.enabled = true
+
+    subtitleLabel.target = self
+    subtitleLabel.clickType = .Single
 
     playButton.image = NSImage(named: "playButton")
     playButton.frame.size.width = 40
@@ -109,6 +137,7 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
   public func setupConstraints() {
     titleLabel.translatesAutoresizingMaskIntoConstraints = false
     subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+    durationLabel.translatesAutoresizingMaskIntoConstraints = false
     playButton.translatesAutoresizingMaskIntoConstraints = false
     trackLabel.translatesAutoresizingMaskIntoConstraints = false
     imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -120,18 +149,28 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
     titleLabel.rightAnchor.constraintEqualToAnchor(titleLabel.superview!.rightAnchor, constant: 10).active = true
 
     subtitleLabel.leftAnchor.constraintEqualToAnchor(imageView.rightAnchor, constant: 10).active = true
-    subtitleLabel.rightAnchor.constraintEqualToAnchor(titleLabel.superview!.rightAnchor, constant: 10).active = true
     subtitleLabel.topAnchor.constraintEqualToAnchor(titleLabel.bottomAnchor).active = true
 
     playButton.rightAnchor.constraintEqualToAnchor(playButton.superview!.rightAnchor, constant: -20).active = true
-    playButton.centerYAnchor.constraintEqualToAnchor(playButton.superview!.centerYAnchor).active = true
+    playButton.topAnchor.constraintEqualToAnchor(playButton.superview!.topAnchor, constant: 4).active = true
+
+    durationLabel.centerXAnchor.constraintEqualToAnchor(playButton.centerXAnchor).active = true
+    durationLabel.topAnchor.constraintEqualToAnchor(playButton.bottomAnchor, constant: 3).active = true
 
     imageView.leftAnchor.constraintEqualToAnchor(trackLabel.rightAnchor).active = true
     imageView.centerYAnchor.constraintEqualToAnchor(imageView.superview!.centerYAnchor).active = true
   }
 
-  override public func hitTest(aPoint: NSPoint) -> NSView? {
-    return nil
+  func clickAlbum(sender: NSEvent) {
+    guard let item = item else { return }
+
+    AppDelegate.navigate(item.meta("album-urn", ""), fragments: item.meta("album-fragments", [:]))
+  }
+
+  func clickArtist(sender: NSEvent) {
+    guard let item = item else { return }
+
+    AppDelegate.navigate(item.meta("artist-urn", ""), fragments: item.meta("artist-fragments", [:]))
   }
 
   public required init?(coder: NSCoder) {
@@ -147,7 +186,12 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
 
     trackLabel.stringValue = item.meta("trackNumber", "")
     titleLabel.stringValue = item.title
-    subtitleLabel.stringValue = item.subtitle
+
+    let attributedString = NSMutableAttributedString(string: item.subtitle)
+    attributedString.addAttribute(NSLinkAttributeName, value: "https://www.hackingwithswift.com", range: NSRange(location: 0, length: item.subtitle.length))
+    subtitleLabel.attributedStringValue = attributedString
+    subtitleLabel.sizeToFit()
+    durationLabel.stringValue = NSTimeInterval(item.meta("duration", 0)).minutesAndSeconds
 
     if item.subtitle.isPresent {
       titleLabel.centerYAnchor.constraintEqualToAnchor(titleLabel.superview!.centerYAnchor, constant: -10).active = true
@@ -155,19 +199,23 @@ public class TrackRow: NSTableRowView, SpotConfigurable {
       titleLabel.centerYAnchor.constraintEqualToAnchor(titleLabel.superview!.centerYAnchor).active = true
     }
 
+    if !item.meta("artist-urn", "").isEmpty {
+      subtitleLabel.clickAction = #selector(TrackRow.clickArtist(_:))
+    } else {
+      subtitleLabel.clickAction = nil
+    }
+
     self.item = item
 
-    if item.image.isPresent {
-      if item.image.hasPrefix("http") {
-        imageView.heightAnchor.constraintEqualToConstant(40).active = true
-        imageView.widthAnchor.constraintEqualToConstant(40).active = true
-        imageView.setImage(NSURL(string: item.image))
-      } else {
-        imageView.image = NSImage(named: item.image)
-        imageView.heightAnchor.constraintEqualToConstant(18).active = true
-        imageView.widthAnchor.constraintEqualToConstant(18).active = true
-        imageView.tintColor = selected ? NSColor.whiteColor() : NSColor.grayColor()
-      }
+    if item.image.isPresent && item.image.hasPrefix("http") {
+      imageView.heightAnchor.constraintEqualToConstant(40).active = true
+      imageView.widthAnchor.constraintEqualToConstant(40).active = true
+      imageView.setImage(NSURL(string: item.image))
+    } else {
+      imageView.image = NSImage(named: item.image)
+      imageView.heightAnchor.constraintEqualToConstant(18).active = true
+      imageView.widthAnchor.constraintEqualToConstant(18).active = true
+      imageView.tintColor = selected ? NSColor.whiteColor() : NSColor.grayColor()
     }
 
     if item.meta("playing", type: Bool.self) == true {
