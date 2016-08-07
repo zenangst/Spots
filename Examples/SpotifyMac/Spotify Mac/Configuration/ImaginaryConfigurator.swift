1558b6db15a4d30597c8e98880f3607cd9aa7188
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-07 20:19:46 +0200

    Add animation when loading images from network

--- Examples/SpotifyMac/Spotify Mac/Configuration/ImaginaryConfigurator.swift
+++ Examples/SpotifyMac/Spotify Mac/Configuration/ImaginaryConfigurator.swift
@@ -3,11 +3,17 @@ import Imaginary
 struct ImaginaryConfigurator: Configurator {
 
   func configure() {
-    
     Imaginary.Configuration.preConfigure = nil
     Imaginary.Configuration.postConfigure = nil
+
     Imaginary.Configuration.transitionClosure = { imageView, image in
       imageView.image = image
+      imageView.layer?.opacity = 1.0
+      let animation = CABasicAnimation(keyPath: "opacity")
+      animation.duration = 0.3
+      animation.fromValue = 0.0
+      animation.toValue = 1.0
+      imageView.layer?.addAnimation(animation, forKey: "fade")
     }
   }
 }
