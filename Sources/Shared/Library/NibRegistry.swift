37ca83f5e6984c155701b9d11ebeec27c5a95657
Khoa Pham <onmyway133@gmail.com>
2016-08-09 09:00:53 +0200

    Add NibRegistry

---
+++ Sources/Shared/Library/NibRegistry.swift
@@ -0,0 +1,21 @@
+import Brick
+
+/// A nib registry that is used internally when resolving kind to the corresponding spot.
+public struct NibRegistry {
+  /// A Key-value dictionary of registred types
+  var storage = [String : Nib]()
+
+  /**
+   A subscripting method for getting a value from storage using a StringConvertible key
+
+   - Returns: An optional Nib
+   */
+  public subscript(key: StringConvertible) -> Nib? {
+    get {
+      return storage[key.string]
+    }
+    set(value) {
+      storage[key.string] = value
+    }
+  }
+}
