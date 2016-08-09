608e17c32301b8f5078a5134a5e66acdbc498ca2
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-09 08:23:19 +0200

    Refactor register method

--- Examples/SpotifyMac/Spotify Mac/Configuration/BlueprintConfigurator.swift
+++ Examples/SpotifyMac/Spotify Mac/Configuration/BlueprintConfigurator.swift
@@ -6,16 +6,16 @@ import Tailor
 struct BlueprintConfigurator: Configurator {
 
   func configure() {
-    blueprints[AlbumBlueprint.key] = AlbumBlueprint.drawing
-    blueprints[AlbumsBlueprint.key] = AlbumsBlueprint.drawing
-    blueprints[ArtistBlueprint.key] = ArtistBlueprint.drawing
-    blueprints[BrowseBlueprint.key] = BrowseBlueprint.drawing
-    blueprints[CategoryBlueprint.key] = CategoryBlueprint.drawing
-    blueprints[FollowingBlueprint.key] = FollowingBlueprint.drawing
-    blueprints[PlaylistBlueprint.key] = PlaylistBlueprint.drawing
-    blueprints[PlaylistsBlueprint.key] = PlaylistsBlueprint.drawing
-    blueprints[SongsBlueprint.key] = SongsBlueprint.drawing
-    blueprints[TopArtistsBlueprint.key] = TopArtistsBlueprint.drawing
-    blueprints[TopTracksBlueprint.key] = TopTracksBlueprint.drawing
+    blueprints.register(AlbumBlueprint.self)
+    blueprints.register(AlbumsBlueprint.self)
+    blueprints.register(ArtistBlueprint.self)
+    blueprints.register(BrowseBlueprint.self)
+    blueprints.register(CategoryBlueprint.self)
+    blueprints.register(FollowingBlueprint.self)
+    blueprints.register(PlaylistBlueprint.self)
+    blueprints.register(PlaylistsBlueprint.self)
+    blueprints.register(SongsBlueprint.self)
+    blueprints.register(TopArtistsBlueprint.self)
+    blueprints.register(TopTracksBlueprint.self)
   }
 }
