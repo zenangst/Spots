0a324ee6c169d23858797986032f8a8d278fe790
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-13 21:44:13 +0200

    Use min()

--- Sources/Shared/Models/Component.swift
+++ Sources/Shared/Models/Component.swift
@@ -72,8 +72,8 @@ public struct Component: Mappable {
 
     let JSONItems: [JSONDictionary]
 
-    if let amountOfItems = amountOfItems where amountOfItems <= items.count {
-      JSONItems = Array(items[0..<amountOfItems]).map { $0.dictionary }
+    if let amountOfItems = amountOfItems {
+      JSONItems = Array(items[0..<min(amountOfItems, items.count)]).map { $0.dictionary }
     } else {
       JSONItems = items.map { $0.dictionary }
     }
