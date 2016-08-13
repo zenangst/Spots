8be269e41c5012a98b2cb7b3de7c802c52a34c09
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-13 12:11:56 +0200

    Remove legacy code

--- Sources/Mac/Extensions/Listable+Mac.swift
+++ Sources/Mac/Extensions/Listable+Mac.swift
@@ -16,40 +16,6 @@ extension Listable {
     }
   }
 
-  public func prepare() {
-    var cached: View?
-    for (index, item) in component.items.enumerate() {
-      prepareItem(item, index: index, cached: &cached)
-    }
-    cached = nil
-  }
-
-  public func prepareItem(item: ViewModel, index: Int, inout cached: View?) {
-    cachedViewFor(item, cache: &cached)
-
-    component.items[index].index = index
-
-    guard let view = cached as? SpotConfigurable else { return }
-
-    view.configure(&component.items[index])
-
-    if component.items[index].size.height == 0.0 {
-      component.items[index].size.height = view.size.height
-    }
-
-    if component.items[index].size.width == 0.0 {
-      component.items[index].size.width = view.size.width
-    }
-  }
-
-  func cachedViewFor(item: ViewModel, inout cache: View?) {
-    let reuseIdentifer = item.kind.isPresent ? item.kind : component.kind
-    let componentClass = self.dynamicType.views.storage[reuseIdentifer] ?? self.dynamicType.defaultView
-
-    if cache?.isKindOfClass(componentClass) == false { cache = nil }
-    if cache == nil { cache = componentClass.init() }
-  }
-
   func configureLayout(component: Component) {
     let top: CGFloat = component.meta("insetTop", 0.0)
     let left: CGFloat = component.meta("insetLeft", 0.0)
