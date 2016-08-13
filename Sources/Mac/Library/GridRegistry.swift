694f75633e3e067c3bb4c5817296d16dfa3c17cf
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-13 16:04:01 +0200

    Refactor GridRegistry

--- Sources/Mac/Library/GridRegistry.swift
+++ Sources/Mac/Library/GridRegistry.swift
@@ -3,15 +3,31 @@ import Brick
 
 /// A view registry that is used internally when resolving kind to the corresponding spot.
 public struct GridRegistry {
+
+  public enum Item {
+    case classType(NSCollectionViewItem.Type)
+    case nib(Nib)
+  }
+
   /// A Key-value dictionary of registred types
-  var storage = [String : NSCollectionViewItem.Type]()
+  var storage = [String : Item]()
+
+  var defaultItem: Item? {
+    didSet {
+      storage[defaultIdentifier] = defaultItem
+    }
+  }
+
+  var defaultIdentifier: String {
+    return String(defaultItem)
+  }
 
   /**
    A subscripting method for getting a value from storage using a StringConvertible key
 
    - Returns: An optional UIView type
    */
-  public subscript(key: StringConvertible) -> NSCollectionViewItem.Type? {
+  public subscript(key: StringConvertible) -> Item? {
     get {
       return storage[key.string]
     }
