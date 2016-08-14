5281c5b80818098bb211e9042839b00ebdb30273
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-14 18:30:16 +0200

    Add purge and make to GridRegistry

--- Sources/Mac/Library/GridRegistry.swift
+++ Sources/Mac/Library/GridRegistry.swift
@@ -35,4 +35,46 @@ public struct GridRegistry {
       storage[key.string] = value
     }
   }
+
+  // MARK: - Template
+
+  private var cache: NSCache = NSCache()
+
+  func purge() {
+    cache.removeAllObjects()
+  }
+
+  func make(identifier: String) -> (type: RegistryType?, item: NSCollectionViewItem?) {
+    guard let item = storage[identifier] else { return (type: nil, item: nil) }
+
+    let registryType: RegistryType?
+    var view: NSCollectionViewItem? = nil
+
+    switch item {
+    case .classType(let classType):
+      registryType = .Regular
+      if let item = cache.objectForKey(registryType!.rawValue + identifier) as? NSCollectionViewItem {
+        return (type: registryType, item: item)
+      }
+
+      view = classType.init()
+
+    case .nib(let nib):
+      registryType = .Nib
+      if let view = cache.objectForKey(registryType!.rawValue + identifier) as? NSCollectionViewItem {
+        return (type: registryType, item: view)
+      }
+
+      var views: NSArray?
+      if nib.instantiateWithOwner(nil, topLevelObjects: &views) {
+        view = views?.filter({ $0 is NSCollectionViewItem }).first as? NSCollectionViewItem
+      }
+    }
+
+    if let view = view {
+      cache.setObject(view, forKey: identifier)
+    }
+
+    return (type: registryType, item: view)
+  }
 }
