400c63110fc0670809da72d6777d31aee7ed38a9
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-27 20:30:45 +0200

    Move collection view method to tvOS

---
+++ Sources/tvOS/Extensions/CollectionAdapter+tvOS.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+extension CollectionAdapter {
+  /**
+   Asks the delegate if the specified item should be selected.
+
+   - Parameter collectionView: The collection view object that is asking whether the selection should change.
+   - Parameter indexPath: The index path of the cell to be selected.
+   - Returns: true if the item should be selected or false if it should not.
+   */
+  public func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+    if let indexPath = collectionView.indexPathsForSelectedItems()?.first {
+      collectionView.deselectItemAtIndexPath(indexPath, animated: true)
+      return false
+    } else {
+      return true
+    }
+  }
+}
