f1b803fb8f85d8b87e056dd40fa695e95dd02ea5
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-30 00:56:02 +0200

    Add key label in parse method

--- Sources/Shared/Library/Parser.swift
+++ Sources/Shared/Library/Parser.swift
@@ -9,8 +9,8 @@ public struct Parser {
    - Parameter json: A JSON dictionary of components and items
    - Returns: A collection of spotable objects
    */
-  public static func parse(json: JSONDictionary) -> [Spotable] {
-    guard let components = json["components"] as? JSONArray else { return [] }
+  public static func parse(json: JSONDictionary, key: String = "components") -> [Spotable] {
+    guard let components = json[key] as? JSONArray else { return [] }
 
     return components.map {
       SpotFactory.resolve(Component($0))
