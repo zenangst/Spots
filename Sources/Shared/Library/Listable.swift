0bc11b93ae332b529d27f697319f03460dfc7e87
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-30 00:59:06 +0200

    Add headers to Listable

--- Sources/Shared/Library/Listable.swift
+++ Sources/Shared/Library/Listable.swift
@@ -1,5 +1,9 @@
 /// Gridable is protocol for Spots that are based on UICollectionView
 public protocol Listable: Spotable {
 
+  #if !os(OSX)
+  static var headers: Registry { get set }
+  #endif
+
   var tableView: TableView { get }
 }
