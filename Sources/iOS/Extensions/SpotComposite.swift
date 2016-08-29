74d96c3bcdf681feb9f802d93a3d95c77fd85e6f
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-30 00:53:19 +0200

    Add SpotComposite protocol

---
+++ Sources/iOS/Extensions/SpotComposite.swift
@@ -0,0 +1,38 @@
+import UIKit
+import Brick
+
+public protocol SpotComposite: class {
+  weak var spotsDelegate: SpotsDelegate? { get set }
+  var contentView: UIView { get }
+
+  func configure(inout item: ViewModel, spots: [Spotable]?)
+  func parse(item: ViewModel) -> [Spotable]
+}
+
+public extension SpotComposite where Self : UIView {
+
+  func configure(inout item: ViewModel, spots: [Spotable]?) {
+    guard let spots = spots else { return }
+
+    var height: CGFloat = 0.0
+    spots.enumerate().forEach { index, spot in
+      spot.component.size = CGSize(
+        width: contentView.frame.width,
+        height: ceil(spot.render().height))
+      spot.setup(contentView.frame.size)
+      contentView.addSubview(spot.render())
+      spot.render().layoutIfNeeded()
+      height += spot.render().contentSize.height
+    }
+
+    item.size.height = height
+  }
+}
+
+public extension SpotComposite {
+
+  public func parse(item: ViewModel) -> [Spotable] {
+    let spots = Parser.parse(item.meta, key: "children")
+    return spots
+  }
+}
