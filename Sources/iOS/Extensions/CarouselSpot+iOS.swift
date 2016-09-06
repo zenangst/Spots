c7d57222a370f4212903531307223922b08dec96
Christoffer Winterkvist <christoffer@hyper.no>
2016-09-06 12:30:13 +0200

    Add dynamic span implement to sizeForItemAt(indexPath)

--- Sources/iOS/Extensions/CarouselSpot+iOS.swift
+++ Sources/iOS/Extensions/CarouselSpot+iOS.swift
@@ -10,12 +10,19 @@ extension CarouselSpot {
   }
 
   public func sizeForItemAt(indexPath: NSIndexPath) -> CGSize {
+    guard indexPath.item < component.items.count else { return CGSize.zero }
     var width = collectionView.width
 
     if component.span > 0 {
-      width = collectionView.width / CGFloat(component.span)
-      width -= layout.sectionInset.left / component.span
-      width -= layout.minimumInteritemSpacing
+      if dynamicSpan && CGFloat(component.items.count) < component.span  {
+        width = collectionView.width / CGFloat(component.items.count)
+        width -= layout.sectionInset.left / CGFloat(component.items.count)
+        width -= layout.minimumInteritemSpacing
+      } else {
+        width = collectionView.width / CGFloat(component.span)
+        width -= layout.sectionInset.left / component.span
+        width -= layout.minimumInteritemSpacing
+      }
     }
 
     component.items[indexPath.item].size.width = width
