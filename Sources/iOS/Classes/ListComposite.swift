b87f477efa1d8d7b86b8e2cbcd0d24fe632ba303
Christoffer Winterkvist <christoffer@hyper.no>
2016-09-07 17:25:38 +0200

    Clean up imports and document composite views

--- Sources/iOS/Classes/ListComposite.swift
+++ Sources/iOS/Classes/ListComposite.swift
@@ -1,8 +1,11 @@
 import UIKit
-import Brick
 
+/// A proxy cell that is used for composite views inside other Spotable objects
 class ListComposite: UITableViewCell, SpotComposable {
 
+  /**
+   Performs any clean up necessary to prepare the view for use again.
+   */
   override func prepareForReuse() {
     contentView.subviews.forEach { $0.removeFromSuperview() }
   }
