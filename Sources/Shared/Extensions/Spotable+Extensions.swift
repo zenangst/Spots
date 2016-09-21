2b1eacece9b5f19f3a05c027abe62d50ef3b467a
Christoffer Winterkvist <christoffer@hyper.no>
2016-09-21 10:35:39 +0200

    Remove registering defaultView in storage

--- Sources/Shared/Extensions/Spotable+Extensions.swift
+++ Sources/Shared/Extensions/Spotable+Extensions.swift
@@ -302,6 +302,5 @@ public extension Spotable {
 
   public static func register(defaultView view: View.Type) {
     self.views.defaultItem = Registry.Item.classType(view)
-    self.views.storage[self.views.defaultIdentifier] = Registry.Item.classType(view)
   }
 }
