37444e3e347275b1e35be01d3ae4999f6109e89c
Christoffer Winterkvist <christoffer@hyper.no>
2016-09-26 11:51:17 +0200

    Fix Hue related warnings

--- Examples/SpotsNibDemo/SpotsNibDemo/AppDelegate.swift
+++ Examples/SpotsNibDemo/SpotsNibDemo/AppDelegate.swift
@@ -10,28 +10,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     window = UIWindow(frame: UIScreen.mainScreen().bounds)
-    window?.backgroundColor = UIColor.hex("13151A")
+    window?.backgroundColor = UIColor(hex: "13151A")
 
     UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
 
     let navigationBar = UINavigationBar.appearance()
     navigationBar.translucent = false
-    navigationBar.barTintColor = UIColor.hex("181B23")
-    navigationBar.tintColor = UIColor.hex("181B23")
+    navigationBar.barTintColor = UIColor(hex: "181B23")
+    navigationBar.tintColor = UIColor(hex: "181B23")
     navigationBar.titleTextAttributes = [
-      NSForegroundColorAttributeName: UIColor.hex("465771")
+      NSForegroundColorAttributeName: UIColor(hex: "465771")
     ]
     navigationBar.setBackgroundImage(nil, forBarMetrics: .Default)
 
     ListSpot.register(nib: UINib(nibName: "CustomList", bundle: NSBundle.mainBundle()), identifier: "custom-list")
     ListSpot.configure = { tableView in
-      tableView.backgroundColor = UIColor.hex("181B23")
+      tableView.backgroundColor = UIColor(hex: "181B23")
       tableView.separatorInset = UIEdgeInsets(
         top: 0, left: 7.5,
         bottom: 0, right: 7.5)
       tableView.layoutMargins = UIEdgeInsetsZero
       tableView.tableFooterView = UIView(frame: CGRect.zero)
-      tableView.separatorColor = UIColor.hex("465771")
+      tableView.separatorColor = UIColor(hex: "465771")
     }
 
     let component = Component(title: "Browse", items: [
