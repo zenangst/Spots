3d6a4991b7d8514099cea2381511f548e1993016
Christoffer Winterkvist <christoffer@hyper.no>
2016-09-29 11:59:35 +0200

    Call layoutSubviews in didAddSubviewToContainer

--- Sources/iOS/Classes/SpotsScrollView.swift
+++ Sources/iOS/Classes/SpotsScrollView.swift
@@ -96,6 +96,7 @@ public class SpotsScrollView: UIScrollView {
     scrollView.addObserver(self, forKeyPath: ObservedKeypath.contentOffset.rawValue, options: .Old, context: subviewContext)
 
     setNeedsLayout()
+    layoutSubviews()
   }
 
   /**
@@ -216,7 +217,7 @@ public class SpotsScrollView: UIScrollView {
       self.frame.size.height = superview.frame.size.height
     }
 
-    guard initialContentOffset != contentOffset else { return }
+    guard !CGPointEqualToPoint(initialContentOffset, contentOffset) else { return }
     setNeedsLayout()
     layoutIfNeeded()
   }
