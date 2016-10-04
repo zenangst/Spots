f9b29be635d92c6fdc4238263be73559a9c0fd4d
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-04 07:53:52 +0200

    dispatch_source_t -> DispatchSourceFileSystemObject

--- Sources/iOS/Classes/SpotsController.swift
+++ Sources/iOS/Classes/SpotsController.swift
@@ -63,7 +63,7 @@ open class SpotsController: UIViewController, SpotsProtocol, SpotsCompositeDeleg
   /// A dispatch queue is a lightweight object to which your application submits blocks for subsequent execution.
   public let fileQueue: DispatchQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.default)
   /// An identifier for the type system object being monitored by a dispatch source.
-  public var source: dispatch_source_t!
+  public var source: DispatchSourceFileSystemObject!
   #endif
 
   /// An optional SpotCache used for view controller caching
