b04d5601ff15770ba646da09d33e80a845d5fec6
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-04 10:58:43 +0200

    Add script to SpotsNibDemo podfile

---
+++ core-pods.rb
@@ -0,0 +1,25 @@
+def core_pods
+  pod 'Spots', path: '../../'
+  pod 'Brick', :git => 'https://github.com/hyperoslo/Brick', :branch => 'swift-3'
+  pod 'Tailor', '2.0.0'
+  pod 'CryptoSwift', '0.6.0'
+  pod 'Cache', :git => 'https://github.com/hyperoslo/Cache', :branch => 'swift-3'
+
+  post_install do |installer|
+    puts("Enabling dev mode for Spots")
+    Dir.glob(File.join("Pods", "**", "Pods*{debug,Private}.xcconfig")).each do |file|
+      File.open(file, 'a') { |f| f.puts "\nDEBUG_INFORMATION_FORMAT = dwarf" }
+    end
+    installer.pods_project.targets.each do |target|
+      if target.name == 'Spots'
+        target.build_configurations.each do |config|
+          if config.name == 'Debug'
+            config.build_settings['OTHER_SWIFT_FLAGS'] = '-DDEBUG -DDEVMODE'
+            else
+            config.build_settings['OTHER_SWIFT_FLAGS'] = ''
+          end
+        end
+      end
+    end
+  end
+end
