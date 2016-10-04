130511e302d3c2cb731aea2213a13727883ef90f
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-04 13:05:48 +0200

    Fix tvOS extension in Spots

--- Sources/tvOS/Extensions/CollectionAdapter+tvOS.swift
+++ Sources/tvOS/Extensions/CollectionAdapter+tvOS.swift
@@ -8,7 +8,7 @@ extension CollectionAdapter {
    - parameter indexPath: The index path of the cell to be selected.
    - returns: true if the item should be selected or false if it should not.
    */
-  public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+  @objc(collectionView:shouldSelectItemAtIndexPath:) public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
     if let indexPath = collectionView.indexPathsForSelectedItems?.first {
       collectionView.deselectItem(at: indexPath, animated: true)
       return false
