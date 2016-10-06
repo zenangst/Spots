7bf244a7ea903fd455998c4c199e910a39273ff8
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-06 17:22:40 +0200

    Update documentation for SpotCache

--- Sources/Shared/Structs/SpotCache.swift
+++ Sources/Shared/Structs/SpotCache.swift
@@ -4,9 +4,11 @@ import CryptoSwift
 
 /// A SpotCache struct used for Controller and Spotable object caching
 public struct SpotCache {
-
+  /// A unique identifer string for the SpotCache
   public let key: String
-  static let cacheName = "SpotCache"
+  /// The cache name used by Cache
+  static let cacheName = String(describing: SpotCache.self)
+  /// A JSON Cache object
   let cache = Cache<JSON>(name: "\(SpotCache.cacheName)/\(Bundle.main.bundleIdentifier!)")
 
   /// The path of the cache
@@ -21,48 +23,40 @@ public struct SpotCache {
 
   // MARK: - Initialization
 
-  /**
-   Initialize a SpotCache with a unique cache key
-
-   - parameter key: A string that is used as an identifier for the SpotCache
-   */
+  /// Initialize a SpotCache with a unique cache key
+  ///
+  /// - parameter key: A string that is used as an identifier for the SpotCache
+  ///
+  /// - returns: A SpotCache object
   public init(key: String) {
     self.key = key
   }
 
   // MARK: - Cache
 
-  /**
-   Save JSON to the SpotCache
-
-   - parameter json: A JSON object
-   */
+  /// Save JSON to the SpotCache
+  ///
+  /// - parameter json: A JSON object
   public func save(_ json: [String : Any]) {
     let expiry = Expiry.date(Date().addingTimeInterval(60 * 60 * 24 * 3))
     SyncCache(cache).add(key, object: JSON.dictionary(json), expiry: expiry)
   }
 
-  /**
-   Load JSON from cache
-
-   - returns: A Swift dictionary
-   */
+  /// Load JSON from cache
+  ///
+  /// - returns: A Swift dictionary
   public func load() -> [String : Any] {
     return SyncCache(cache).object(key)?.object as? [String : Any] ?? [:]
   }
 
-  /**
-   Clear the current SpotCache
-   */
+  /// Clear the current SpotCache
   public func clear() {
     cache.remove(key)
   }
 
-  /**
-   The SpotCache file name
-
-   - returns: An md5 representation of the SpotCache's file name, computed from the SpotCache key
-   */
+  /// The SpotCache file name
+  ///
+  /// - returns: An md5 representation of the SpotCache's file name, computed from the SpotCache key
   func fileName() -> String {
     if let digest = key.data(using: String.Encoding.utf8)?.md5() {
       var string = ""
