ec25fe8709a7cd777f78a11ccbde0fc481015aa7
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-07 07:41:08 +0200

    Migrate docs on Composable

--- Sources/iOS/Protocols/Composable.swift
+++ Sources/iOS/Protocols/Composable.swift
@@ -6,20 +6,16 @@ public protocol Composable: class {
   /// A required content view, needed because of Composable extensions
   var contentView: View { get }
 
-  /**
-   Configure a Composable with an item and a collection of Spotable objects
-
-   - parameter item:  The Item struct that is Composable
-   - parameter spots: A collection of Spotable objects that should be used to configure the child
-   */
+  /// Configure a Composable with an item and a collection of Spotable objects
+  ///
+  /// - parameter item:  The Item struct that is Composable
+  /// - parameter spots: A collection of Spotable objects that should be used to configure the child
   func configure(_ item: inout Item, spots: [Spotable]?)
 
-  /**
-   Parse children of an Item into Spotable components
-
-   - parameter item: The item you want to parse
-
-   - returns: A collection of Spotable objects
-   */
+  /// Parse children of an Item into Spotable components
+  ///
+  /// - parameter item: The item you want to parse
+  ///
+  /// - returns: A collection of Spotable objects
   func parse(_ item: Item) -> [Spotable]
 }
