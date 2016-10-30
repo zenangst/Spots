2e12d2699296f70a62efef5e65884f573e729f29
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-30 14:03:43 +0100

    Add SpotableTests

---
+++ SpotsTests/Shared/TestSpotable.swift
@@ -0,0 +1,45 @@
+@testable import Spots
+import Foundation
+import XCTest
+import Brick
+
+class SpotableTests : XCTestCase {
+
+  func testAppendingMultipleItemsToSpot() {
+    let listSpot = ListSpot(component: Component(title: "Component"))
+    listSpot.setup(UIScreen.main.bounds.size)
+    var items: [Item] = []
+
+    for i in 0..<10 {
+      items.append(Item(title: "Item: \(i)"))
+    }
+
+    measure {
+      for _ in 0..<5 {
+        listSpot.append(items)
+        listSpot.render().layoutSubviews()
+      }
+    }
+    XCTAssertEqual(listSpot.items.count, 500)
+  }
+
+  func testAppendingMultipleItemsToSpotInController() {
+    let controller = Controller(spots: [ListSpot(component: Component(title: "Component"))])
+    controller.preloadView()
+    controller.viewDidAppear()
+    var items: [Item] = []
+
+    for i in 0..<10 {
+      items.append(Item(title: "Item: \(i)"))
+    }
+
+    measure {
+      for _ in 0..<5 {
+        controller.append(items, spotIndex: 0, withAnimation: .automatic, completion: nil)
+        controller.spots.forEach { $0.render().layoutSubviews() }
+      }
+    }
+    XCTAssertEqual(controller.spots[0].items.count, 500)
+  }
+
+}
