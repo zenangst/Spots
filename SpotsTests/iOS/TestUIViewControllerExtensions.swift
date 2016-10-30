7797a62db124c267c4a14937176aa22d9584ccc3
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-30 14:03:38 +0100

    Add UIViewControllerExtensionsTests

---
+++ SpotsTests/iOS/TestUIViewControllerExtensions.swift
@@ -0,0 +1,28 @@
+@testable import Spots
+import Foundation
+import XCTest
+import Brick
+
+class UIViewControllerExtensionsTests: XCTestCase {
+
+  var controller: Controller!
+
+  override func setUp() {
+    controller = Controller(spots: [])
+  }
+
+  override func tearDown() {
+    controller = nil
+  }
+
+  func testShouldAutorotateOnController() {
+    XCTAssertEqual(controller.spots_shouldAutorotate(), true)
+  }
+
+  func testShouldAutorotateOnChildController() {
+    let parentController = UIViewController()
+    parentController.addChildViewController(controller)
+    XCTAssertEqual(controller.spots_shouldAutorotate(), true)
+  }
+
+}
