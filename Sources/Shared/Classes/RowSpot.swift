0eb7cbe1b36647a58f376bc10ebcaf7ecd118311
Christoffer Winterkvist <christoffer@hyper.no>
2016-11-06 11:32:44 +0100

    Add RowSpot class

---
+++ Sources/Shared/Classes/RowSpot.swift
@@ -0,0 +1,19 @@
+/// A RowSpot, a collection view based Spotable object that lays out its items in a vertical order based of the item sizes
+class RowSpot: GridSpot {
+
+  /// A required initializer to instantiate a RowSpot with a component.
+  ///
+  /// - parameter component: A component.
+  ///
+  /// - returns: An initialized row spot with component.
+  required init(component: Component) {
+    var component = component
+    component.span = 1
+
+    if component.kind.isEmpty {
+      component.kind = Component.Kind.Row.string
+    }
+
+    super.init(component: component)
+  }
+}
