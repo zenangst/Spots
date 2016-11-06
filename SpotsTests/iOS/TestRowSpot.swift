118c98fba9d1be363cf21330a81f0c26866760b6
Christoffer Winterkvist <christoffer@hyper.no>
2016-11-06 19:47:41 +0100

    Improve testing for RowSpot

--- SpotsTests/iOS/TestRowSpot.swift
+++ SpotsTests/iOS/TestRowSpot.swift
@@ -5,6 +5,20 @@ import XCTest
 
 class RowSpotTests: XCTestCase {
 
+  var spot: RowSpot!
+  var cachedSpot: RowSpot!
+
+  override func setUp() {
+    spot = RowSpot(component: Component())
+    cachedSpot = RowSpot(cacheKey: "cached-row-spot")
+  }
+
+  override func tearDown() {
+    spot = nil
+    cachedSpot.stateCache?.clear()
+    cachedSpot = nil
+  }
+
   func testConvenienceInitWithTitleAndKind() {
     let spot = RowSpot(title: "Spot")
 
@@ -55,5 +69,36 @@ class RowSpotTests: XCTestCase {
     XCTAssertEqual(rowSpot.identifier(at: indexPath), "custom-item-kind")
 
     RowSpot.views.storage.removeAll()
+  }
+
+  func testSpotCollectionDelegate() {
+    let items = [Item(title: "Test item")]
+    let spot = RowSpot(component: Component(items: items))
+    spot.render().frame.size = CGSize(width: 100, height: 100)
+    spot.render().layoutSubviews()
+    
+    let cell = spot.collectionView.cellForItem(at: IndexPath(item: 0, section: 0))
+    XCTAssertEqual(cell!.frame.size, CGSize(width: UIScreen.main.bounds.width, height: 44))
+  }
+
+  func testSpotCache() {
+    cachedSpot.stateCache?.clear()
+
+    let item = Item(title: "test")
+
+    XCTAssertEqual(cachedSpot.component.items.count, 0)
+    cachedSpot.append(item) {
+      self.cachedSpot.cache()
+    }
+
+    var exception: XCTestExpectation? = self.expectation(description: "Wait for cache")
+    Dispatch.delay(for: 0.25) {
+      let cachedSpot = RowSpot(cacheKey: self.cachedSpot.stateCache!.key)
+      XCTAssertEqual(cachedSpot.component.items.count, 1)
+      cachedSpot.stateCache?.clear()
+      exception?.fulfill()
+      exception = nil
+    }
+    waitForExpectations(timeout: 0.5, handler: nil)
   }
 }
