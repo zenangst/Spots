ddecd2b771d0aff40a1e69d1931c1adb132bcf28
Christoffer Winterkvist <christoffer@hyper.no>
2016-11-08 13:43:49 +0100

    Add additional tests for Spotable delegate

--- SpotsTests/iOS/TestDelegate.swift
+++ SpotsTests/iOS/TestDelegate.swift
@@ -14,6 +14,8 @@ class TestDelegate: SpotsDelegate {
 
 class DelegateTests: XCTestCase {
 
+  // MARK: - UICollectionView
+
   func testCollectionViewDelegateSelection() {
     let delegate = TestDelegate()
     let spot = GridSpot(component: Component(items: [
@@ -28,6 +30,14 @@ class DelegateTests: XCTestCase {
     spot.spotDelegate.collectionView(spot.collectionView, didSelectItemAt: IndexPath(item: 1, section: 0))
     XCTAssertEqual(delegate.countsInvoked, 1)
   }
+
+  func testCollectionViewCanFocus() {
+    let spot = GridSpot(component: Component(items: [Item(title: "title 1")]))
+    XCTAssertEqual(spot.spotDelegate.collectionView(spot.collectionView, canFocusItemAt: IndexPath(item: 0, section: 0)), true)
+    XCTAssertEqual(spot.spotDelegate.collectionView(spot.collectionView, canFocusItemAt: IndexPath(item: 1, section: 0)), false)
+  }
+
+  // MARK: - UITableView
 
   func testTableViewDelegateSelection() {
     let delegate = TestDelegate()
@@ -44,15 +54,44 @@ class DelegateTests: XCTestCase {
     XCTAssertEqual(delegate.countsInvoked, 1)
   }
 
-  func testCollectionViewCanFocus() {
-    let spot = GridSpot(component: Component(items: [Item(title: "title 1")]))
-    XCTAssertEqual(spot.spotDelegate.collectionView(spot.collectionView, canFocusItemAt: IndexPath(item: 0, section: 0)), true)
-    XCTAssertEqual(spot.spotDelegate.collectionView(spot.collectionView, canFocusItemAt: IndexPath(item: 1, section: 0)), false)
-  }
-
-  func testHeightForRowOnListable() {
+  func testTableViewHeightForRowOnListable() {
     let spot = ListSpot(component: Component(items: [Item(title: "title 1")]))
     XCTAssertEqual(spot.spotDelegate.tableView(spot.tableView, heightForRowAt: IndexPath(row: 0, section: 0)), 44.0)
     XCTAssertEqual(spot.spotDelegate.tableView(spot.tableView, heightForRowAt: IndexPath(row: 1, section: 0)), 0.0)
+  }
+
+  func testDelegateTitleForHeader() {
+    ListSpot.register(header: CustomListHeaderView.self, identifier: "list")
+    let spot = ListSpot(component: Component(
+      title: "title",
+      header: "list",
+      items: [
+        Item(title: "title 1"),
+        Item(title: "title 2")
+      ]))
+    spot.render().frame.size = CGSize(width: 100, height: 100)
+    spot.render().layoutSubviews()
+
+    var view = spot.spotDelegate.tableView(spot.tableView, viewForHeaderInSection: 0)
+    XCTAssert(view is CustomListHeaderView)
+
+    /// Expect to return nil if header is in use.
+    var title = spot.spotDelegate.tableView(spot.tableView, titleForHeaderInSection: 0)
+    XCTAssertEqual(title, nil)
+
+    /// Expect to return title if header is empty.
+    spot.component.header = ""
+    title = spot.spotDelegate.tableView(spot.tableView, titleForHeaderInSection: 0)
+    XCTAssertEqual(title, spot.component.title)
+
+    /// Expect to return nil if title and header is empty.
+    spot.component.title = ""
+    title = spot.spotDelegate.tableView(spot.tableView, titleForHeaderInSection: 0)
+    XCTAssertEqual(title, nil)
+
+    view = spot.spotDelegate.tableView(spot.tableView, viewForHeaderInSection: 0)
+    XCTAssertEqual(view, nil)
+
+
   }
 }
