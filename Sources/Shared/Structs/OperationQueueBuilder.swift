f0b30666b2e294eb91c7abf45f561b0468054652
Christoffer Winterkvist <christoffer@hyper.no>
2016-11-12 19:11:02 +0100

    Change quality of service to user initiated

--- Sources/Shared/Structs/OperationQueueBuilder.swift
+++ Sources/Shared/Structs/OperationQueueBuilder.swift
@@ -5,6 +5,7 @@ struct OperationQueueBuilder {
   static func build() -> OperationQueue {
     let operationQueue = OperationQueue()
     operationQueue.maxConcurrentOperationCount = 1
+    operationQueue.qualityOfService = .userInitiated
     return operationQueue
   }
 }
