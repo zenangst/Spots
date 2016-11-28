bb921c2ca4a9143c702ab640516dd18ad300fdb1
Christoffer Winterkvist <christoffer@hyper.no>
2016-11-28 21:11:54 +0100

    Add endDisplay to SpotsDelegates

--- Sources/Shared/Protocols/SpotsDelegates.swift
+++ Sources/Shared/Protocols/SpotsDelegates.swift
@@ -48,6 +48,12 @@ public protocol SpotsDelegate: class {
   /// - parameter item: The data for the view that is going to be displayed.
   /// - parameter spot: An object that conforms to the spotable protocol.
   func willDisplay(item: Item, in spot: Spotable)
+
+  /// A delegate method that is triggered when ever a view will no longer be displayed.
+  ///
+  /// - parameter item: The data for the view that is going to be displayed.
+  /// - parameter spot: An object that conforms to the spotable protocol.
+  func endDisplay(item: Item, in spot: Spotable)
 }
 
 // MARK: - SpotsDelegate extension
@@ -65,6 +71,7 @@ public extension SpotsDelegate {
   func didChange(spots: [Spotable]) {}
 
   func willDisplay(item: Item, in spot: Spotable) {}
+  func endDisplay(item: Item, in spot: Spotable) {}
 }
 
 /// A refresh delegate for handling reloading of a Spot
