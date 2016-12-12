5136f3cb484ef9a6592c79bbbf0b21faf03d0184
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-12 07:59:26 +0100

    Add CompositeSpot

---
+++ Sources/Shared/Classes/CompositeSpot.swift
@@ -0,0 +1,21 @@
+public struct CompositeSpot: Equatable {
+  /// Returns a Boolean value indicating whether two values are equal.
+  ///
+  /// Equality is the inverse of inequality. For any values `a` and `b`,
+  /// `a == b` implies that `a != b` is `false`.
+  ///
+  /// - Parameters:
+  ///   - lhs: A value to compare.
+  ///   - rhs: Another value to compare.
+  public static func ==(lhs: CompositeSpot, rhs: CompositeSpot) -> Bool {
+    return lhs.spot.component == rhs.spot.component &&
+      lhs.spotableIndex == rhs.spotableIndex &&
+      lhs.itemIndex == rhs.itemIndex
+  }
+
+
+  var parentSpot: Spotable?
+  var spot: Spotable
+  var spotableIndex: Int
+  var itemIndex: Int
+}
