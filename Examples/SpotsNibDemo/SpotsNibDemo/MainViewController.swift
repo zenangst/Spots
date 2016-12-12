08393909649909a45e023208a42ca8a143570035
Aashish Dhawan <aashishdhawan@gmail.com>
2016-12-12 20:51:26 +0530

    Remove header

--- Examples/SpotsNibDemo/SpotsNibDemo/MainViewController.swift
+++ Examples/SpotsNibDemo/SpotsNibDemo/MainViewController.swift
@@ -1,11 +1,3 @@
-//
-//  MainViewController.swift
-//  SpotsNibDemo
-//
-//  Created by Aashish Dhawan on 12/12/16.
-//  Copyright © 2016 Hyper. All rights reserved.
-//
-
 import Foundation
 import UIKit
 import Spots
