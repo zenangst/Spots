ac8579b5f35a175c907c79e203556ad91a5ae147
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-18 18:04:24 +0100

    Add new example

---
+++ Examples/Composition/Composition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Composition
+//
+//  Created by Christoffer Winterkvist on 12/18/16.
+//  Copyright © 2016 Christoffer Winterkvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
