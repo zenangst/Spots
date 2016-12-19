ca8e51c4bd0fe039e4ddf11a2d081a2d132896e1
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-19 19:25:38 +0100

    Refactor dummy payload

--- Examples/Composition/Composition/CompositionController.swift
+++ Examples/Composition/Composition/CompositionController.swift
@@ -1,21 +1,36 @@
 import Spots
 import Brick
 
-class CompositionController : Controller {
+class CompositionController : Controller, SpotsDelegate {
 
   static var components: [Component] {
-    let json: [String : Any] = [
+
+    let items: [[String : Any]] = [
+      ["title" : "foo", "kind" : "view", "size" : ["height" : 88.0]],
+      ["title" : "bar", "kind" : "view", "size" : ["height" : 88.0]],
+      ["title" : "baz", "kind" : "view", "size" : ["height" : 88.0]]
+    ]
+
+    let json1: [String : Any] = [
       "kind" : "composite",
       "children" : [
         [
           "kind" : "list",
           "header" : "header",
           "title" : "foo bar baz",
-          "items" : [
-            ["title" : "foo", "kind" : "view", "size" : ["height" : 88.0]],
-            ["title" : "bar", "kind" : "view", "size" : ["height" : 88.0]],
-            ["title" : "baz", "kind" : "view", "size" : ["height" : 88.0]],
-          ]
+          "items" : items
+        ]
+      ]
+    ]
+
+    let json2: [String : Any] = [
+      "kind" : "composite",
+      "children" : [
+        [
+          "kind" : "list",
+          "header" : "header",
+          "title" : "foo bar baz",
+          "items" : items
         ]
       ]
     ]
@@ -25,11 +40,14 @@ class CompositionController : Controller {
         kind : Component.Kind.Grid.string,
         span: 2,
         items: [
-          Item(json),
-          Item(json)
+          Item(json1),
+          Item(json2)
         ]
       )]
     return components
   }
 
+  func didSelect(item: Item, in spot: Spotable) {
+    print(item)
+  }
 }
