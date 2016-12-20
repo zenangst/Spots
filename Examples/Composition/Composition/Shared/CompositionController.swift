ef7cc11d92f46d9a115b4b45ff6fa759bce3ef50
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-20 15:54:28 +0100

    Update mock payload

--- Examples/Composition/Composition/Shared/CompositionController.swift
+++ Examples/Composition/Composition/Shared/CompositionController.swift
@@ -5,10 +5,21 @@ class CompositionController : Controller, SpotsDelegate {
 
   static var components: [Component] {
 
-    let items: [[String : Any]] = [
-      ["title" : "foo", "kind" : "view", "size" : ["height" : 88.0]],
-      ["title" : "bar", "kind" : "view", "size" : ["height" : 88.0]],
-      ["title" : "baz", "kind" : "view", "size" : ["height" : 88.0]]
+    let items1: [[String : Any]] = [
+      ["title" : "First", "kind" : "view", "size" : ["height" : 66.0]],
+      ["title" : "Second", "kind" : "view", "size" : ["height" : 66.0]],
+      ["title" : "Third", "kind" : "view", "size" : ["height" : 66.0]]
+    ]
+    let items2: [[String : Any]] = [
+      ["title" : "Forth", "kind" : "view", "size" : ["height" : 66.0]],
+      ["title" : "Fifth", "kind" : "view", "size" : ["height" : 66.0]],
+      ["title" : "Sixt", "kind" : "view", "size" : ["height" : 66.0]]
+    ]
+
+    let items3: [[String : Any]] = [
+      ["title" : "Seventh", "kind" : "view", "size" : ["height" : 66.0]],
+      ["title" : "Eight", "kind" : "view", "size" : ["height" : 66.0]],
+      ["title" : "Ninth", "kind" : "view", "size" : ["height" : 66.0]]
     ]
 
     let json1: [String : Any] = [
@@ -17,8 +28,8 @@ class CompositionController : Controller, SpotsDelegate {
         [
           "kind" : "list",
           "header" : "header",
-          "title" : "foo bar baz",
-          "items" : items
+          "title" : "First Header",
+          "items" : items1
         ]
       ]
     ]
@@ -29,19 +40,33 @@ class CompositionController : Controller, SpotsDelegate {
         [
           "kind" : "list",
           "header" : "header",
-          "title" : "foo bar baz",
-          "items" : items
+          "title" : "Second Header",
+          "items" : items2
+        ]
+      ]
+    ]
+
+    let json3: [String : Any] = [
+      "kind" : "composite",
+      "children" : [
+        [
+          "kind" : "list",
+          "header" : "header",
+          "title" : "Third Header",
+          "items" : items3
         ]
       ]
     ]
 
     let components = [
       Component(
+        title: "The very first Header",
+        header: "header",
         kind: Component.Kind.List.string,
         items: [
-          Item(title: "foo", size: CGSize(width: 0, height: 88)),
-          Item(title: "bar", size: CGSize(width: 0, height: 88)),
-          Item(title: "baz", size: CGSize(width: 0, height: 88))
+          Item(title: "foo", kind: "view", size: CGSize(width: 0, height: 66)),
+          Item(title: "bar", kind: "view", size: CGSize(width: 0, height: 66)),
+          Item(title: "baz", kind: "view", size: CGSize(width: 0, height: 66))
         ]
       ),
       Component(
@@ -58,7 +83,7 @@ class CompositionController : Controller, SpotsDelegate {
         items: [
           Item(json1),
           Item(json2),
-          Item(json1)
+          Item(json3)
         ]
       )
     ]
