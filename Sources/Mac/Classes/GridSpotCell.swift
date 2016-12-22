54aae912c3b5b119fb2fe6d71c67381ea0b61aac
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-22 20:07:38 +0100

    Fix GridSpotCell crashing on macOS

--- Sources/Mac/Classes/GridSpotCell.swift
+++ Sources/Mac/Classes/GridSpotCell.swift
@@ -5,6 +5,10 @@ class GridSpotCell: NSCollectionViewItem, SpotConfigurable {
 
   var preferredViewSize: CGSize = CGSize(width: 0, height: 120)
 
+  open override func loadView() {
+    view = NSView()
+  }
+
   func configure(_ item: inout Item) {
     view.wantsLayer = true
     view.layer?.backgroundColor = NSColor.red.cgColor
