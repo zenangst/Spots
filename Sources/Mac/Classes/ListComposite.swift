8986a6fd1d3d97f97e5fe825edca0f6223030fe3
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-22 19:51:55 +0100

    Clean up composite views on macOS

--- Sources/Mac/Classes/ListComposite.swift
+++ Sources/Mac/Classes/ListComposite.swift
@@ -1,21 +1,15 @@
-import Brick
 import Cocoa
+import Brick
 
 /// A proxy cell that is used for composite views inside other Spotable objects
-public class GridComposite: NSCollectionViewItem, Composable {
+public class ListComposite: NSTableRowView, Composable {
 
   /// A required content view, needed because of Composable extensions
   public var contentView: View {
-    return view
+    return self
   }
-
-  open lazy var customView: FlippedView = FlippedView()
 
   static open var isFlipped: Bool {
     get { return true }
-  }
-
-  open override func loadView() {
-    view = customView
   }
 }
