27efee5c24349e7bda8fb4fcba55aec194bc45f3
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-22 20:08:00 +0100

    Fix CarouselSpotCell crashing on macOS

--- Sources/Mac/Classes/CarouselSpotCell.swift
+++ Sources/Mac/Classes/CarouselSpotCell.swift
@@ -5,6 +5,10 @@ class CarouselSpotCell: NSCollectionViewItem, SpotConfigurable {
 
   var preferredViewSize: CGSize = CGSize(width: 0, height: 120)
 
+  open override func loadView() {
+    view = NSView()
+  }
+
   func configure(_ item: inout Item) {
     view.wantsLayer = true
     view.layer?.backgroundColor = NSColor.red.cgColor
