aa1ad6569bf5e7d3951c622b1bf54bcc59369083
Christoffer Winterkvist <christoffer@winterkvist.com>
2016-12-22 20:02:40 +0100

    Refactor AppDelegate on macOS

--- Examples/Composition/Composition/macOS/AppDelegate.swift
+++ Examples/Composition/Composition/macOS/AppDelegate.swift
@@ -7,12 +7,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   var windowController = NSWindowController(window: NSWindow())
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    windowController.window?.styleMask = [.closable, .borderless, .miniaturizable, .resizable, .titled, .fullSizeContentView]
-    windowController.window?.minSize = NSSize(width: 960, height: 640)
     windowController.windowFrameAutosaveName = "Window"
 
+    windowController.window?.styleMask = [.closable, .borderless, .miniaturizable, .resizable, .titled, .fullSizeContentView]
+    windowController.window?.minSize = NSSize(width: 640, height: 650)
+    windowController.window?.titlebarAppearsTransparent = true
+    windowController.window?.isMovable = true
+    windowController.window?.isOpaque = false
+    windowController.window?.titleVisibility = .hidden
+    windowController.window?.backgroundColor = NSColor(red:1.0, green:1.0, blue:1.0, alpha: 0.985)
+
+    CarouselSpot.register(view: FeaturedCell.self, identifier: "featured")
+    CarouselSpot.register(view: GridCell.self, identifier: "default")
+
+    GridSpot.register(view: FeaturedCell.self, identifier: "featured")
+    GridSpot.register(view: GridCell.self, identifier: "default")
+    GridSpot.register(defaultView: GridCell.self)
+
     ListSpot.register(view: CompositionListView.self, identifier: "view")
-    ListSpot.register(defaultView: CompositionListView.self)
+    ListSpot.register(view: ListCell.self, identifier: "default")
+    ListSpot.register(defaultView: ListCell.self)
+
+    CarouselSpot.configure = { collectionView in
+      collectionView.backgroundView?.layer?.backgroundColor = NSColor.white.cgColor
+    }
+    
+    GridSpot.configure = { collectionView in
+      collectionView.backgroundView?.layer?.backgroundColor = NSColor.white.cgColor
+    }
+
+    ListSpot.configure = { tableView in
+      tableView.backgroundColor = NSColor.white
+    }
+
+    Spots.Controller.configure = { scrollView in
+      scrollView.backgroundColor = NSColor.white
+    }
 
     let controller = CompositionController()
     windowController.contentViewController = controller
