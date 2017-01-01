efd859bdec92cfa1f4a2fa4604b28f632186562d
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-01-02 00:03:35 +0100

    Improve layout code for Listable

--- Sources/iOS/Extensions/Listable+Extensions+iOS.swift
+++ Sources/iOS/Extensions/Listable+Extensions+iOS.swift
@@ -16,11 +16,11 @@ public extension Listable {
   /// - parameter size: A CGSize to set the width of the table view
   ///
   public func layout(_ size: CGSize) {
-    tableView.frame.size.width = size.width - (tableView.contentInset.left + tableView.contentInset.right)
+    tableView.frame.size.width = size.width - (tableView.contentInset.left)
     tableView.frame.origin.x = size.width / 2 - tableView.frame.width / 2
 
-    guard let componentSize = component.size else { return }
-    tableView.frame.size.height = componentSize.height
+//    guard let componentSize = component.size else { return }
+//    tableView.frame.size.height = componentSize.height
   }
 
   /// Scroll to Item matching predicate
