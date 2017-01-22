892f80bac22d7e1ee361fc0b258192b3a625397e
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-01-22 19:39:38 +0100

    Add Paginate and UserInteraction structs

---
+++ Sources/Shared/Structs/UserInteraction.swift
@@ -0,0 +1,37 @@
+import Foundation
+import Tailor
+
+public struct UserInteraction: Mappable {
+
+  enum Key: String {
+    case paginate = "paginate"
+  }
+
+  var paginate: Paginate = .disabled
+
+  static let rootKey: String = "user-interaction"
+
+  public var dictionary: [String : Any] {
+    return [
+      Key.paginate.rawValue: paginate.rawValue
+    ]
+  }
+
+  public init(_ map: [String : Any] = [:]) {
+    configure(withJSON: map)
+  }
+
+  public mutating func configure(withJSON map: [String : Any]) {
+    if let paginate: String = map.property(Key.paginate.rawValue) {
+      self.paginate <- Paginate(rawValue: paginate)
+    }
+  }
+
+  public static func == (lhs: UserInteraction, rhs: UserInteraction) -> Bool {
+    return lhs.paginate == rhs.paginate
+  }
+
+  public static func != (lhs: UserInteraction, rhs: UserInteraction) -> Bool {
+    return !(lhs == rhs)
+  }
+}
