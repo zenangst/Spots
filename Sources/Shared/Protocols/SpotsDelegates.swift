28a90c6db34334734c470a6cf0ea85b1d603013e
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-01-22 12:17:18 +0100

    Update docs

--- Sources/Shared/Protocols/SpotsDelegates.swift
+++ Sources/Shared/Protocols/SpotsDelegates.swift
@@ -72,6 +72,7 @@ public protocol RefreshDelegate: class {
 
   /// A delegate method for when your spot controller was refreshed using pull to refresh
   ///
+  /// - parameter spots: A collection of Spotable objects
   /// - parameter refreshControl: A UIRefreshControl
   /// - parameter completion: A completion closure that should be triggered when the update is completed
   #if os(iOS)
