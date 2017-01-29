1850a31427eb9301d2d9c35287526a68ab5e428f
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-01-29 19:36:19 +0100

    Refactor ListWrapper to be Wrappable

--- Sources/iOS/Classes/ListWrapper.swift
+++ Sources/iOS/Classes/ListWrapper.swift
@@ -1,33 +1,16 @@
 import UIKit
 
-class ListWrapper: UITableViewCell {
+class ListWrapper: UITableViewCell, Wrappable {
 
-  weak var view: View?
-
-  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-    super.init(style: .default, reuseIdentifier: reuseIdentifier)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  func configure(with view: View) {
-    if let previousView = self.view {
-      previousView.removeFromSuperview()
-    }
-
-    contentView.addSubview(view)
-    self.view = view
-  }
+  weak var wrappedView: View?
 
   override func layoutSubviews() {
     super.layoutSubviews()
 
-    self.view?.frame = contentView.bounds
+    self.wrappedView?.frame = contentView.bounds
   }
 
   override func prepareForReuse() {
-    view?.removeFromSuperview()
+    wrappedView?.removeFromSuperview()
   }
 }
