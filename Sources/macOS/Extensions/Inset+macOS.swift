7126ad89e6c50f6321f5a71553a655d49991acc9
Vadym Markov <markov.vadym@gmail.com>
2017-01-30 20:09:58 +0100

    Remove unused functions

--- Sources/macOS/Extensions/Inset+macOS.swift
+++ Sources/macOS/Extensions/Inset+macOS.swift
@@ -11,14 +11,4 @@ extension Inset {
     scrollView.contentInsets.bottom = CGFloat(self.bottom)
     scrollView.contentInsets.right = CGFloat(self.right)
   }
-
-  /// Configure section insets on flow layouts
-  ///
-  /// - Parameter layout: The flow layout that should be configured.
-  func configure(layout: FlowLayout) {
-    layout.sectionInset.top = CGFloat(self.top)
-    layout.sectionInset.left = CGFloat(self.left)
-    layout.sectionInset.bottom = CGFloat(self.bottom)
-    layout.sectionInset.right = CGFloat(self.right)
-  }
 }
