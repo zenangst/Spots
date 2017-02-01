b7f266b177e5b9d5ccc053cb50eb064e1990ad89
Vadym Markov <markov.vadym@gmail.com>
2017-02-01 16:26:11 +0100

    Implement delegate proxy

--- RxSpots/SpotsDelegate+Rx.swift
+++ RxSpots/SpotsDelegate+Rx.swift
@@ -1,9 +1,21 @@
-//
-//  SpotsDelegate+Rx.swift
-//  Spots
-//
-//  Created by Vadym Markov on 01/02/2017.
-//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
-//
+import Spots
+import Brick
+import RxSwift
+import RxCocoa
 
-import Foundation
+// MARK: - Delegate proxy
+
+final class SpotDelegateProxy: DelegateProxy, DelegateProxyType, SpotsDelegate {
+
+  class func currentDelegateFor(_ object: AnyObject) -> AnyObject? {
+    return (object as? Spotable)?.delegate ?? (object as? Controller)?.delegate
+  }
+
+  class func setCurrentDelegate(_ delegate: AnyObject?, toObject object: AnyObject) {
+    if let spot = object as? Spotable {
+      spot.delegate = delegate as? SpotsDelegate
+    } else if let controller = object as? Controller {
+      controller.delegate = delegate as? SpotsDelegate
+    }
+  }
+}
