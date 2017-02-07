8b32b0b496ad0c61788b2e751804beeb064d9a20
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-07 11:03:13 +0100

    Add additional helper views

--- SpotsTests/Shared/Helpers.swift
+++ SpotsTests/Shared/Helpers.swift
@@ -47,6 +47,124 @@ struct Helper {
 }
 
 #if !os(OSX)
+
+  class HeaderView: UIView, SpotConfigurable, Componentable {
+
+    public var preferredHeaderHeight: CGFloat = 50.0
+
+    var preferredViewSize: CGSize = CGSize(width: 200, height: 50)
+
+    lazy var titleLabel: UILabel = {
+      let label = UILabel()
+      label.textAlignment = .center
+      return label
+    }()
+
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      addSubview(titleLabel)
+
+      configureConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+
+    func configureConstraints() {
+      titleLabel.translatesAutoresizingMaskIntoConstraints = false
+      titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+      titleLabel.leftAnchor.constraint(equalTo: titleLabel.superview!.leftAnchor).isActive = true
+      titleLabel.rightAnchor.constraint(equalTo: titleLabel.superview!.rightAnchor).isActive = true
+      titleLabel.centerYAnchor.constraint(equalTo: titleLabel.superview!.centerYAnchor).isActive = true
+    }
+
+    func configure(_ item: inout Item) {
+      titleLabel.text = item.title
+    }
+
+    func configure(_ component: Component) {
+      titleLabel.text = component.title
+    }
+  }
+
+  class FooterView: UIView, SpotConfigurable, Componentable {
+
+    var preferredHeaderHeight: CGFloat = 50
+
+    var preferredViewSize: CGSize = CGSize(width: 200, height: 50)
+
+    lazy var titleLabel: UILabel = {
+      let label = UILabel()
+      label.textAlignment = .center
+      return label
+    }()
+
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      addSubview(titleLabel)
+
+      configureConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+
+    func configureConstraints() {
+      titleLabel.translatesAutoresizingMaskIntoConstraints = false
+      titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+      titleLabel.leftAnchor.constraint(equalTo: titleLabel.superview!.leftAnchor).isActive = true
+      titleLabel.rightAnchor.constraint(equalTo: titleLabel.superview!.rightAnchor).isActive = true
+      titleLabel.centerYAnchor.constraint(equalTo: titleLabel.superview!.centerYAnchor).isActive = true
+    }
+
+    func configure(_ item: inout Item) {
+      titleLabel.text = item.title
+    }
+
+    func configure(_ component: Component) {
+      titleLabel.text = "This is a footer"
+    }
+  }
+
+  class TextView: UIView, SpotConfigurable {
+
+    var preferredViewSize: CGSize = CGSize(width: 200, height: 50)
+
+    lazy var titleLabel: UILabel = {
+      let label = UILabel()
+      label.textAlignment = .center
+      return label
+    }()
+
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      addSubview(titleLabel)
+
+      backgroundColor = UIColor.gray.withAlphaComponent(0.25)
+
+      configureConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+
+    func configureConstraints() {
+      titleLabel.translatesAutoresizingMaskIntoConstraints = false
+      titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+      titleLabel.leftAnchor.constraint(equalTo: titleLabel.superview!.leftAnchor).isActive = true
+      titleLabel.rightAnchor.constraint(equalTo: titleLabel.superview!.rightAnchor).isActive = true
+      titleLabel.centerYAnchor.constraint(equalTo: titleLabel.superview!.centerYAnchor).isActive = true
+    }
+
+    func configure(_ item: inout Item) {
+      titleLabel.text = item.title
+    }
+  }
+
+
   class CustomListCell: UITableViewCell, SpotConfigurable {
 
     var preferredViewSize: CGSize = CGSize(width: 0, height: 44)
