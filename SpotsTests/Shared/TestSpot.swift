5f8681dfdacc12822e94f2eab3a6a8ff02b05bda
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-07 13:45:25 +0100

    Add Spot test for checking default values

--- SpotsTests/Shared/TestSpot.swift
+++ SpotsTests/Shared/TestSpot.swift
@@ -1,8 +1,25 @@
 @testable import Spots
+import Brick
 import XCTest
 
 class TestSpot: XCTestCase {
 
-  
+  override func setUp() {
+    Configuration.views.storage = [:]
+    Configuration.views.defaultItem = nil
+  }
 
+  func testDefaultValues() {
+    let items = [Item(title: "A"), Item(title: "B")]
+    let component = Component(items: items, hybrid: true)
+    let spot = Spot(component: component)
+
+    spot.setup(CGSize(width: 100, height: 100))
+
+    XCTAssertTrue(spot.view is TableView)
+    XCTAssertTrue(spot.view.isEqual(spot.tableView))
+    XCTAssertEqual(spot.items[0].size,    CGSize(width: 414, height: 44))
+    XCTAssertEqual(spot.items[1].size,    CGSize(width: 414, height: 44))
+    XCTAssertEqual(spot.view.contentSize, CGSize(width: 100, height: 88))
+  }
 }
