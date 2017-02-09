5830a8a5b848ddb761c5990250a47e51679adecc
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-09 14:22:50 +0100

    Add TestSpot to iOS

---
+++ SpotsTests/iOS/TestSpot.swift
@@ -0,0 +1,68 @@
+@testable import Spots
+import Brick
+import XCTest
+
+class TestSpot: XCTestCase {
+
+  override func setUp() {
+    Configuration.views.storage = [:]
+    Configuration.views.defaultItem = nil
+    Configuration.register(view: HeaderView.self, identifier: "Header")
+    Configuration.register(view: TextView.self,   identifier: "TextView")
+    Configuration.register(view: FooterView.self, identifier: "Footer")
+  }
+
+  func testDefaultValues() {
+    let items = [Item(title: "A"), Item(title: "B")]
+    let component = Component(items: items, hybrid: true)
+    let spot = Spot(component: component)
+
+    spot.setup(CGSize(width: 100, height: 100))
+
+    XCTAssertTrue(spot.view is TableView)
+    XCTAssertTrue(spot.view.isEqual(spot.tableView))
+    XCTAssertEqual(spot.items[0].size,    CGSize(width: UIScreen.main.bounds.width, height: 44))
+    XCTAssertEqual(spot.items[1].size,    CGSize(width: UIScreen.main.bounds.width, height: 44))
+    XCTAssertEqual(spot.view.contentSize, CGSize(width: 100, height: 88))
+  }
+
+  func testCompareHybridListSpotWithCoreType() {
+    let items = [Item(title: "A"), Item(title: "B")]
+    let component = Component(kind: Component.Kind.list.string, items: items, hybrid: true)
+    let listComponent = Component(kind: Component.Kind.list.string, items: items)
+    let spot = Spot(component: component)
+    let listSpot = ListSpot(component: listComponent)
+
+    XCTAssertTrue(type(of: spot.view) == type(of: listSpot.view))
+
+    spot.setup(CGSize(width: 100, height: 100))
+    listSpot.setup(CGSize(width: 100, height: 100))
+    listSpot.layout(CGSize(width: 100, height: 100))
+
+    XCTAssertEqual(spot.items[0].size, listSpot.items[0].size)
+    XCTAssertEqual(spot.items[1].size, listSpot.items[0].size)
+    XCTAssertEqual(spot.view.frame, listSpot.view.frame)
+    XCTAssertEqual(spot.view.contentSize, listSpot.view.contentSize)
+  }
+
+  func testHybridListSpotWithHeaderAndFooter() {
+    let component = Component(
+      header: "Header",
+      footer: "Footer",
+      kind: Component.Kind.list.string,
+      layout: Layout(span: 2.0),
+      items: [
+        Item(title: "A"),
+        Item(title: "B"),
+        Item(title: "C"),
+        Item(title: "D")
+      ],
+      hybrid: true
+    )
+    let spot = Spot(component: component)
+    spot.setup(CGSize(width: 100, height: 100))
+
+    XCTAssertEqual(spot.view.frame.size, CGSize(width: 100, height: 100))
+    XCTAssertEqual(spot.view.contentSize, CGSize(width: 100, height: 176))
+  }
+}
