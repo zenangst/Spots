5deacc65bc797b1b3709f4cdf34ac630968a08a9
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-10 11:39:50 +0100

    Refactor configure(spot:) into configure(collectionViewLayout:)

--- Sources/iOS/Extensions/Layout+iOS.swift
+++ Sources/iOS/Extensions/Layout+iOS.swift
@@ -14,15 +14,15 @@ extension Layout {
     spot.layout.minimumLineSpacing = CGFloat(lineSpacing)
   }
 
-  public func configure(spot: Spot) {
-    spot.collectionViewLayout?.sectionInset = UIEdgeInsets(
+  public func configure(collectionViewLayout: CollectionLayout) {
+    collectionViewLayout.sectionInset = UIEdgeInsets(
       top: CGFloat(inset.top),
       left: CGFloat(inset.left),
       bottom: CGFloat(inset.bottom),
       right: CGFloat(inset.right)
     )
 
-    spot.collectionViewLayout?.minimumInteritemSpacing = CGFloat(itemSpacing)
-    spot.collectionViewLayout?.minimumLineSpacing = CGFloat(lineSpacing)
+    collectionViewLayout.minimumInteritemSpacing = CGFloat(itemSpacing)
+    collectionViewLayout.minimumLineSpacing = CGFloat(lineSpacing)
   }
 }
