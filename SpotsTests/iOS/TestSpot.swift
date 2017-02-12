2b791a2855a1cee262ff4b4e17dd288666bde98a
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-12 19:55:40 +0100

    Refactor test expectations

--- SpotsTests/iOS/TestSpot.swift
+++ SpotsTests/iOS/TestSpot.swift
@@ -20,8 +20,8 @@ class TestSpot: XCTestCase {
 
     XCTAssertTrue(spot.view is TableView)
     XCTAssertTrue(spot.view.isEqual(spot.tableView))
-    XCTAssertEqual(spot.items[0].size, CGSize(width: UIScreen.main.bounds.width, height: 44))
-    XCTAssertEqual(spot.items[1].size, CGSize(width: UIScreen.main.bounds.width, height: 44))
+    XCTAssertEqual(spot.items[0].size, CGSize(width: 100, height: 44))
+    XCTAssertEqual(spot.items[1].size, CGSize(width: 100, height: 44))
     XCTAssertEqual(spot.view.frame.size, CGSize(width: 100, height: 100))
 
     /// tvOS adds 14 pixels to each item in a table view.
