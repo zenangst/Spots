9cdc5745253bbaeb58efbdf8c2c128c9061bb5ff
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-15 11:10:48 +0100

    Implement configureWrappedView for ListWrapper

--- Sources/iOS/Classes/ListWrapper.swift
+++ Sources/iOS/Classes/ListWrapper.swift
@@ -4,6 +4,12 @@ class ListWrapper: UITableViewCell, Wrappable {
 
   weak var wrappedView: View?
 
+  func configureWrappedView() {
+    if let cell = wrappedView as? UITableViewCell {
+      cell.contentView.frame = contentView.frame
+      cell.isUserInteractionEnabled = false
+    }
+  }
   override func layoutSubviews() {
     super.layoutSubviews()
 
