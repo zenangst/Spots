a93d77a83c72efdb3db6cfb3dfc53dac768e93d2
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-15 13:13:34 +0100

    Set backgroundColor to .clear for wrapper views

--- Sources/iOS/Classes/GridWrapper.swift
+++ Sources/iOS/Classes/GridWrapper.swift
@@ -4,10 +4,13 @@ class GridWrapper: UICollectionViewCell, Wrappable {
 
   weak var wrappedView: View?
 
-  override func layoutSubviews() {
-    super.layoutSubviews()
-
-    wrappedView?.frame.size = contentView.bounds.size
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .white
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
 
   func configureWrappedView() {
@@ -15,6 +18,12 @@ class GridWrapper: UICollectionViewCell, Wrappable {
       cell.contentView.frame = contentView.frame
       cell.isUserInteractionEnabled = false
     }
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    wrappedView?.frame.size = contentView.bounds.size
   }
 
   override func prepareForReuse() {
