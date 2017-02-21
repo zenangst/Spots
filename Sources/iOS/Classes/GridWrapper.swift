62fb9717808e1095812c58deb11a3dad82365743
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-21 23:24:05 +0100

    Remove trailing spaces

--- Sources/iOS/Classes/GridWrapper.swift
+++ Sources/iOS/Classes/GridWrapper.swift
@@ -8,7 +8,7 @@ class GridWrapper: UICollectionViewCell, Wrappable, Cell {
     super.init(frame: frame)
     backgroundColor = .white
   }
-  
+
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
