77bff2133ef3f237459c026870979ddc0cd73a1c
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-02-21 23:31:21 +0100

    Extract common code into a method to reduce code duplication

--- Sources/iOS/Extensions/Delegate+iOS+UIScrollView.swift
+++ Sources/iOS/Extensions/Delegate+iOS+UIScrollView.swift
@@ -58,33 +58,27 @@ extension Delegate: UIScrollViewDelegate {
   }
 
   public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-    guard let spot = spot,
-      let collectionView = spot.userInterface as? CollectionView,
-      let collectionViewLayout = collectionView.collectionViewLayout as? CollectionLayout,
-      spot.component.interaction.scrollDirection == .horizontal,
-      spot.component.interaction.paginate != .disabled else {
+    performPaginatedScrolling { spot, collectionView, collectionViewLayout in
+      guard let centerIndexPath = getCenterIndexPath(in: collectionView,
+                                                     scrollView: scrollView,
+                                                     point: scrollView.contentOffset,
+                                                     contentSize: collectionViewLayout.contentSize,
+                                                     offset: collectionViewLayout.minimumInteritemSpacing) else {
+                                                      return
+      }
+
+      guard let item = spot.item(at: centerIndexPath.item) else {
         return
-    }
+      }
 
-    guard let centerIndexPath = getCenterIndexPath(in: collectionView,
-                                                   scrollView: scrollView,
-                                                   point: scrollView.contentOffset,
-                                                   contentSize: collectionViewLayout.contentSize,
-                                                   offset: collectionViewLayout.minimumInteritemSpacing) else {
-                                                    return
-    }
-
-    guard let item = spot.item(at: centerIndexPath.item) else {
-      return
-    }
-
-    switch spot {
-    case let spot as CarouselSpot:
-      spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: true)
-    case let spot as Spot:
-      spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: true)
-    default:
-      break
+      switch spot {
+      case let spot as CarouselSpot:
+        spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: true)
+      case let spot as Spot:
+        spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: true)
+      default:
+        break
+      }
     }
   }
 
@@ -94,6 +88,48 @@ extension Delegate: UIScrollViewDelegate {
   /// - parameter velocity:            The velocity of the scroll view (in points) at the moment the touch was released.
   /// - parameter targetContentOffset: The expected offset when the scrolling action decelerates to a stop.
   public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    performPaginatedScrolling { spot, collectionView, collectionViewLayout in
+      var centerIndexPath: IndexPath?
+
+      centerIndexPath = getCenterIndexPath(in: collectionView,
+                                           scrollView: scrollView,
+                                           point: targetContentOffset.pointee,
+                                           contentSize: collectionViewLayout.contentSize,
+                                           offset: collectionViewLayout.minimumInteritemSpacing)
+
+      if spot.component.interaction.paginate == .page {
+        let widthBounds = scrollView.contentSize.width - scrollView.frame.size.width
+        let isBeyondBounds = targetContentOffset.pointee.x >= widthBounds && centerIndexPath == nil
+
+        if isBeyondBounds {
+          centerIndexPath = IndexPath(item: spot.items.count - 1, section: 0)
+        }
+      }
+
+      guard let foundIndexPath = centerIndexPath else {
+        return
+      }
+
+      guard let centerLayoutAttributes = collectionViewLayout.layoutAttributesForItem(at: foundIndexPath) else {
+        return
+      }
+
+      if let item = spot.item(at: foundIndexPath.item) {
+        switch spot {
+        case let spot as CarouselSpot:
+          spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: false)
+        case let spot as Spot:
+          spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: false)
+        default:
+          break
+        }
+      }
+
+      targetContentOffset.pointee.x = centerLayoutAttributes.frame.midX - scrollView.frame.width / 2
+    }
+  }
+
+  fileprivate func performPaginatedScrolling(_ closure: (_ spot: Spotable, _ collectionView: UICollectionView, _ collectionViewLayout: CollectionLayout) -> Void) {
     guard let spot = spot,
       let collectionView = spot.userInterface as? CollectionView,
       let collectionViewLayout = collectionView.collectionViewLayout as? CollectionLayout,
@@ -102,43 +138,7 @@ extension Delegate: UIScrollViewDelegate {
         return
     }
 
-    var centerIndexPath: IndexPath?
-
-    centerIndexPath = getCenterIndexPath(in: collectionView,
-      scrollView: scrollView,
-      point: targetContentOffset.pointee,
-      contentSize: collectionViewLayout.contentSize,
-      offset: collectionViewLayout.minimumInteritemSpacing)
-
-    if spot.component.interaction.paginate == .page {
-      let widthBounds = scrollView.contentSize.width - scrollView.frame.size.width
-      let isBeyondBounds = targetContentOffset.pointee.x >= widthBounds && centerIndexPath == nil
-
-      if isBeyondBounds {
-        centerIndexPath = IndexPath(item: spot.items.count - 1, section: 0)
-      }
-    }
-
-    guard let foundIndexPath = centerIndexPath else {
-      return
-    }
-
-    guard let centerLayoutAttributes = collectionViewLayout.layoutAttributesForItem(at: foundIndexPath) else {
-      return
-    }
-
-    if let item = spot.item(at: foundIndexPath.item) {
-      switch spot {
-      case let spot as CarouselSpot:
-        spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: false)
-      case let spot as Spot:
-        spot.carouselScrollDelegate?.spotableCarouselDidEndScrolling(spot, item: item, animated: false)
-      default:
-        break
-      }
-    }
-
-    targetContentOffset.pointee.x = centerLayoutAttributes.frame.midX - scrollView.frame.width / 2
+    closure(spot, collectionView, collectionViewLayout)
   }
 
   fileprivate func getCenterIndexPath(in collectionView: UICollectionView, scrollView: UIScrollView, point: CGPoint, contentSize: CGSize, offset: CGFloat) -> IndexPath? {
