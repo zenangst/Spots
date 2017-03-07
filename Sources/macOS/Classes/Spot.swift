25ec52612fde5207b9cfa75d99a626770f17d735
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-07 11:24:02 +0100

    Add implementation for layoutHorizontalCollectionView(_ collectionView, ...)

--- Sources/macOS/Classes/Spot.swift
+++ Sources/macOS/Classes/Spot.swift
@@ -366,7 +366,45 @@ public class Spot: NSObject, Spotable {
   }
 
   fileprivate func layoutHorizontalCollectionView(_ collectionView: CollectionView, with size: CGSize) {
+    guard let collectionViewLayout = collectionView.collectionViewLayout else {
+      return
+    }
 
+    collectionViewLayout.prepare()
+    collectionViewLayout.invalidateLayout()
+
+    if let collectionViewContentSize = collectionView.collectionViewLayout?.collectionViewContentSize {
+
+      let headerHeight = headerView?.frame.size.height ?? 0.0
+      let footerHeight = footerView?.frame.size.height ?? 0.0
+      var newCollectionViewHeight: CGFloat = 0.0
+
+      newCollectionViewHeight <- component.items.sorted(by: {
+        $0.size.height > $1.size.height
+      }).first?.size.height
+
+      var collectionViewContentSize = collectionViewContentSize
+
+      if let layout = component.layout {
+        collectionViewContentSize.width += CGFloat(layout.inset.left)
+      }
+
+      collectionView.frame.origin.y = headerHeight
+      collectionView.frame.size.width = collectionViewContentSize.width
+      collectionView.frame.size.height = newCollectionViewHeight
+
+      documentView.frame.size = collectionView.frame.size
+
+      if let layout = component.layout {
+        documentView.frame.size.width += CGFloat(layout.inset.right)
+      }
+
+      documentView.frame.size.height = collectionView.frame.size.height + headerHeight + footerHeight
+
+      scrollView.frame.size.width = size.width
+      scrollView.frame.size.height = documentView.frame.size.height
+      scrollView.scrollerInsets.bottom = footerHeight
+    }
   }
 
   fileprivate func layoutVerticalCollectionView(_ collectionView: CollectionView, with size: CGSize) {
