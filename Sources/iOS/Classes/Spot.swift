1513eb54f58f31e469c2573692f7972e85a73722
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-07 11:57:18 +0100

    Move configureCollectionViewHeader to extension

--- Sources/iOS/Classes/Spot.swift
+++ Sources/iOS/Classes/Spot.swift
@@ -209,31 +209,6 @@ public class Spot: NSObject, Spotable, SpotHorizontallyScrollable {
     GridSpot.configure?(collectionView, collectionViewLayout)
   }
 
-  fileprivate func configureCollectionViewHeader(_ collectionView: CollectionView, with size: CGSize) {
-    guard let collectionViewLayout = collectionView.collectionViewLayout as? GridableLayout else {
-      return
-    }
-
-    guard !component.header.isEmpty else {
-      return
-    }
-
-    guard let view = Configuration.views.make(component.header)?.view as? Componentable else {
-      return
-    }
-
-    collectionViewLayout.headerReferenceSize.width = collectionView.frame.size.width
-    collectionViewLayout.headerReferenceSize.height = view.frame.size.height
-
-    if collectionViewLayout.headerReferenceSize.width == 0.0 {
-      collectionViewLayout.headerReferenceSize.width = size.width
-    }
-
-    if collectionViewLayout.headerReferenceSize.height == 0.0 {
-      collectionViewLayout.headerReferenceSize.height = view.preferredHeaderHeight
-    }
-  }
-
   fileprivate func layoutCollectionView(_ collectionView: CollectionView, with size: CGSize) {
     prepareItems()
 
