545d7169fd9aa832a7210e5f7f44faeee68cd788
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-07 20:06:51 +0100

    Rename Spotable to CoreComponent

--- Sources/iOS/Extensions/CoreComponent+Extensions+iOS.swift
+++ Sources/iOS/Extensions/CoreComponent+Extensions+iOS.swift
@@ -1,4 +1,5 @@
 extension CoreComponent {
+
   func didScrollHorizontally(handler: (SpotHorizontallyScrollable) -> Void) {
     guard let spot = self as? SpotHorizontallyScrollable,
       model.interaction.scrollDirection == .horizontal else {
