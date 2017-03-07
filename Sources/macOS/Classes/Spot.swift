d2400091c44ab3b648dbd788b7c84eb368b5e88d
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-07 11:49:27 +0100

    Move carousel related methods to extension

--- Sources/macOS/Classes/Spot.swift
+++ Sources/macOS/Classes/Spot.swift
@@ -295,20 +295,6 @@ public class Spot: NSObject, Spotable {
     }
   }
 
-  fileprivate func setupHorizontalCollectionView(_ collectionView: CollectionView, with size: CGSize) {
-    var newCollectionViewHeight: CGFloat = 0.0
-
-    newCollectionViewHeight <- component.items.sorted(by: {
-      $0.size.height > $1.size.height
-    }).first?.size.height
-
-    scrollView.scrollingEnabled = (component.items.count > 1)
-    scrollView.hasHorizontalScroller = (component.items.count > 1)
-
-    collectionView.frame.size.height = newCollectionViewHeight
-    CarouselSpot.configure?(collectionView)
-  }
-
   fileprivate func setupVerticalCollectionView(_ collectionView: CollectionView, with size: CGSize) {
     GridSpot.configure?(collectionView)
   }
@@ -318,45 +304,6 @@ public class Spot: NSObject, Spotable {
       layoutHorizontalCollectionView(collectionView, with: size)
     } else {
       layoutVerticalCollectionView(collectionView, with: size)
-    }
-  }
-
-  fileprivate func layoutHorizontalCollectionView(_ collectionView: CollectionView, with size: CGSize) {
-    guard let collectionViewLayout = collectionView.collectionViewLayout else {
-      return
-    }
-
-    collectionViewLayout.prepare()
-    collectionViewLayout.invalidateLayout()
-
-    if let collectionViewContentSize = collectionView.collectionViewLayout?.collectionViewContentSize {
-      var newCollectionViewHeight: CGFloat = 0.0
-
-      newCollectionViewHeight <- component.items.sorted(by: {
-        $0.size.height > $1.size.height
-      }).first?.size.height
-
-      var collectionViewContentSize = collectionViewContentSize
-
-      if let layout = component.layout {
-        collectionViewContentSize.width += CGFloat(layout.inset.left)
-      }
-
-      collectionView.frame.origin.y = headerHeight
-      collectionView.frame.size.width = collectionViewContentSize.width
-      collectionView.frame.size.height = newCollectionViewHeight
-
-      documentView.frame.size = collectionView.frame.size
-
-      if let layout = component.layout {
-        documentView.frame.size.width += CGFloat(layout.inset.right)
-      }
-
-      documentView.frame.size.height = collectionView.frame.size.height + headerHeight + footerHeight
-
-      scrollView.frame.size.width = size.width
-      scrollView.frame.size.height = documentView.frame.size.height
-      scrollView.scrollerInsets.bottom = footerHeight
     }
   }
 
--- Sources/macOS/Extensions/Spot+macOS+Carousel.swift
+++ Sources/macOS/Extensions/Spot+macOS+Carousel.swift
@@ -1,9 +1,58 @@
-//
-//  Spot+macOS+Carousel.swift
-//  Spots
-//
-//  Created by Christoffer Winterkvist on 3/7/17.
-//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
-//
+import Cocoa
+import Tailor
 
-import Foundation
+extension Spot {
+
+  func setupHorizontalCollectionView(_ collectionView: CollectionView, with size: CGSize) {
+    var newCollectionViewHeight: CGFloat = 0.0
+
+    newCollectionViewHeight <- component.items.sorted(by: {
+      $0.size.height > $1.size.height
+    }).first?.size.height
+
+    scrollView.scrollingEnabled = (component.items.count > 1)
+    scrollView.hasHorizontalScroller = (component.items.count > 1)
+
+    collectionView.frame.size.height = newCollectionViewHeight
+    CarouselSpot.configure?(collectionView)
+  }
+
+  func layoutHorizontalCollectionView(_ collectionView: CollectionView, with size: CGSize) {
+    guard let collectionViewLayout = collectionView.collectionViewLayout else {
+      return
+    }
+
+    collectionViewLayout.prepare()
+    collectionViewLayout.invalidateLayout()
+
+    if let collectionViewContentSize = collectionView.collectionViewLayout?.collectionViewContentSize {
+      var newCollectionViewHeight: CGFloat = 0.0
+
+      newCollectionViewHeight <- component.items.sorted(by: {
+        $0.size.height > $1.size.height
+      }).first?.size.height
+
+      var collectionViewContentSize = collectionViewContentSize
+
+      if let layout = component.layout {
+        collectionViewContentSize.width += CGFloat(layout.inset.left)
+      }
+
+      collectionView.frame.origin.y = headerHeight
+      collectionView.frame.size.width = collectionViewContentSize.width
+      collectionView.frame.size.height = newCollectionViewHeight
+
+      documentView.frame.size = collectionView.frame.size
+
+      if let layout = component.layout {
+        documentView.frame.size.width += CGFloat(layout.inset.right)
+      }
+
+      documentView.frame.size.height = collectionView.frame.size.height + headerHeight + footerHeight
+
+      scrollView.frame.size.width = size.width
+      scrollView.frame.size.height = documentView.frame.size.height
+      scrollView.scrollerInsets.bottom = footerHeight
+    }
+  }
+}
