6b66bfcc5073df6a1e6031c9aedcad6312e00143
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-07 10:00:57 +0100

    Add convenience init(documentView: View?) to NSScrollView

--- Sources/macOS/Extensions/NSScrollView+Extensions.swift
+++ Sources/macOS/Extensions/NSScrollView+Extensions.swift
@@ -2,6 +2,11 @@ import Cocoa
 
 extension NSScrollView {
 
+  convenience init(documentView: View?) {
+    self.init()
+    self.documentView = documentView
+  }
+
   public var contentOffset: CGPoint {
     get {
       return documentVisibleRect.origin
