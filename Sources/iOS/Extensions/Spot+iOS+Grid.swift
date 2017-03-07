9c13494e7b8ed0f15ff5a3dfb081c5d5980d1a0d
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-07 11:54:06 +0100

    Add extension for iOS

---
+++ Sources/iOS/Extensions/Spot+iOS+Grid.swift
@@ -0,0 +1,9 @@
+//
+//  Spot+iOS+Grid.swift
+//  Spots
+//
+//  Created by Christoffer Winterkvist on 3/7/17.
+//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
