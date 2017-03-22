d92cf59c9bc324d117457f38f5b5f6efbaebb34b
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-22 12:45:48 +0100

    Reduce the size of tests by splitting out empty component test

--- SpotsTests/Shared/TestUserInterface.swift
+++ SpotsTests/Shared/TestUserInterface.swift
@@ -3,83 +3,56 @@ import XCTest
 
 class TestUserInterface: XCTestCase {
 
-  func testVisibleViewsOnListComponent() {
+  func testEmptyComponent() {
     let model = ComponentModel(kind: "list")
     let component = Component(model: model)
     
     component.setup(CGSize(width: 100, height: 100))
 
-    /// Expect that the user interface returns zero views because the model has no items.
     XCTAssertEqual(component.userInterface?.visibleViews.count, 0)
+  }
 
+  func testVisibleViewsOnListComponent() {
     let items = [
       Item(title: "foo"),
       Item(title: "bar"),
       Item(title: "baz")
     ]
+    let model = ComponentModel(kind: "list", items: items)
+    let component = Component(model: model)
+    
+    component.setup(CGSize(width: 100, height: 100))
 
-    let expectation = self.expectation(description: "Wait until components has reloaded.")
-
-    component.reloadIfNeeded(items) {
-      /// After the component has reloaded with items, three views should be returned.
-      XCTAssertEqual(component.userInterface?.visibleViews.count, 3)
-      expectation.fulfill()
-    }
-
-    waitForExpectations(timeout: 10.0, handler: nil)
+    XCTAssertEqual(component.userInterface?.visibleViews.count, 3)
   }
 
   func testVisibleViewsOnGridComponent() {
-    let model = ComponentModel(kind: "grid")
-    let component = Component(model: model)
-    
-    component.setup(CGSize(width: 100, height: 100))
-
-    /// Expect that the user interface returns zero views because the model has no items.
-    XCTAssertEqual(component.userInterface?.visibleViews.count, 0)
-
     let items = [
       Item(title: "foo"),
       Item(title: "bar"),
       Item(title: "baz")
     ]
+    let model = ComponentModel(kind: "grid", items: items)
+    let component = Component(model: model)
+    
+    component.setup(CGSize(width: 100, height: 100))
 
-    let expectation = self.expectation(description: "Wait until components has reloaded.")
-
-    component.reloadIfNeeded(items) {
-      /// After the component has reloaded with items, three views should be returned.
-      XCTAssertEqual(component.userInterface?.visibleViews.count, 3)
-      expectation.fulfill()
-    }
-
-    waitForExpectations(timeout: 10.0, handler: nil)
+    XCTAssertEqual(component.userInterface?.visibleViews.count, 3)
   }
 
   func testVisibleViewsOnCarouselComponent() {
-    let layout = Layout(span: 1)
-    let model = ComponentModel(kind: "carousel", layout: layout)
-    let component = Component(model: model)
-    
-    component.setup(CGSize(width: 100, height: 100))
-
-    /// Expect that the user interface returns zero views because the model has no items.
-    XCTAssertEqual(component.userInterface?.visibleViews.count, 0)
-
     let items = [
       Item(title: "foo"),
       Item(title: "bar"),
       Item(title: "baz")
     ]
+    let layout = Layout(span: 1)
+    let model = ComponentModel(kind: "carousel", layout: layout, items: items)
+    let component = Component(model: model)
+    
+    component.setup(CGSize(width: 100, height: 100))
 
-    let expectation = self.expectation(description: "Wait until components has reloaded.")
-
-    component.reloadIfNeeded(items) {
-      /// After the component has loaded, only one of the views should be visisble as span
-      /// is set to 1 item per page in the carousel.
-      XCTAssertEqual(component.userInterface?.visibleViews.count, 1)
-      expectation.fulfill()
-    }
-
-    waitForExpectations(timeout: 10.0, handler: nil)
+    /// Expect to only have one visible view as the carousel as a span set to one.
+    XCTAssertEqual(component.userInterface?.visibleViews.count, 1)
   }
 }
