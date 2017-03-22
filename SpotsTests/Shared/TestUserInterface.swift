310fefc835d7b63629a7744f036264bff0a54941
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-22 12:35:35 +0100

    Add more tests for different types of components

--- SpotsTests/Shared/TestUserInterface.swift
+++ SpotsTests/Shared/TestUserInterface.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class TestUserInterface: XCTestCase {
 
-  func testVisibleViews() {
+  func testVisibleViewsOnListComponent() {
     let model = ComponentModel(kind: "list")
     let component = Component(model: model)
     
@@ -23,6 +23,60 @@ class TestUserInterface: XCTestCase {
     component.reloadIfNeeded(items) {
       /// After the component has reloaded with items, three views should be returned.
       XCTAssertEqual(component.userInterface?.visibleViews.count, 3)
+      expectation.fulfill()
+    }
+
+    waitForExpectations(timeout: 10.0, handler: nil)
+  }
+
+  func testVisibleViewsOnGridComponent() {
+    let model = ComponentModel(kind: "grid")
+    let component = Component(model: model)
+    
+    component.setup(CGSize(width: 100, height: 100))
+
+    /// Expect that the user interface returns zero views because the model has no items.
+    XCTAssertEqual(component.userInterface?.visibleViews.count, 0)
+
+    let items = [
+      Item(title: "foo"),
+      Item(title: "bar"),
+      Item(title: "baz")
+    ]
+
+    let expectation = self.expectation(description: "Wait until components has reloaded.")
+
+    component.reloadIfNeeded(items) {
+      /// After the component has reloaded with items, three views should be returned.
+      XCTAssertEqual(component.userInterface?.visibleViews.count, 3)
+      expectation.fulfill()
+    }
+
+    waitForExpectations(timeout: 10.0, handler: nil)
+  }
+
+  func testVisibleViewsOnCarouselComponent() {
+    let layout = Layout(span: 1)
+    let model = ComponentModel(kind: "carousel", layout: layout)
+    let component = Component(model: model)
+    
+    component.setup(CGSize(width: 100, height: 100))
+
+    /// Expect that the user interface returns zero views because the model has no items.
+    XCTAssertEqual(component.userInterface?.visibleViews.count, 0)
+
+    let items = [
+      Item(title: "foo"),
+      Item(title: "bar"),
+      Item(title: "baz")
+    ]
+
+    let expectation = self.expectation(description: "Wait until components has reloaded.")
+
+    component.reloadIfNeeded(items) {
+      /// After the component has loaded, only one of the views should be visisble as span
+      /// is set to 1 item per page in the carousel.
+      XCTAssertEqual(component.userInterface?.visibleViews.count, 1)
       expectation.fulfill()
     }
 
