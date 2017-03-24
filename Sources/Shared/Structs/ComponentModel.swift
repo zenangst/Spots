d3f595d039f5f19583ac5a9dba07866565a0d281
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-24 12:22:06 +0100

    Move ComponentKind to its own file

--- Sources/Shared/Structs/ComponentModel.swift
+++ Sources/Shared/Structs/ComponentModel.swift
@@ -6,28 +6,6 @@
 
 import Tailor
 
-
-/// An enum for identifing the ComponentModel kind
-public enum ComponentKind: String, Equatable {
-  /// The identifier for CarouselComponent
-  case carousel
-  /// The identifier for GridComponent
-  case grid
-  /// The identifier for ListComponent
-  case list
-  /// The identifier for RowComponent
-  case row
-
-  /// The lowercase raw value of the case
-  public var string: String {
-    return rawValue.lowercased()
-  }
-
-  public static func == (lhs: ComponentKind, rhs: String) -> Bool {
-    return lhs.string == rhs
-  }
-}
-
 /// The ComponentModel struct is used to configure a Component object
 public struct ComponentModel: Mappable, Equatable, DictionaryConvertible {
 
