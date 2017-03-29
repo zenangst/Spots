b42dac98b0d3c761272b36d731a8240ae3b9b174
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-29 21:15:36 +0200

    Optimize afterUpdate method

--- Sources/iOS/Classes/Component.swift
+++ Sources/iOS/Classes/Component.swift
@@ -248,8 +248,15 @@ public class Component: NSObject, ComponentHorizontallyScrollable {
     )
   }
 
+  public func beforeUpdate() {
+  }
+
   /// This method is invoked after mutations has been performed on a component.
   public func afterUpdate() {
-    setup(with: view.frame.size)
+    if compositeComponents.isEmpty {
+      layout(with: view.frame.size)
+    } else {
+      setup(with: view.frame.size)
+    }
   }
 }
