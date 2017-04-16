52833119c96260e8ce47d93fbab948b2aad76e1c
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-04-16 10:26:11 +0200

    Refactor tests to use `setUp`

--- SpotsTests/Shared/TestSpotsControllerManager.swift
+++ SpotsTests/Shared/TestSpotsControllerManager.swift
@@ -3,6 +3,7 @@ import Spots
 
 class TestSpotsControllerManager: XCTestCase {
 
+  var controller: SpotsController!
   let component = Component(model: ComponentModel(kind: .list, items: [
     Item(title: "foo"),
     Item(title: "bar"),
@@ -11,14 +12,15 @@ class TestSpotsControllerManager: XCTestCase {
 
   override func setUp() {
     Configuration.registerDefault(view: TestView.self)
+
+    controller = SpotsController(components: [component])
+    controller.prepareController()
   }
 
   func testAppendItem() {
     let expectation = self.expectation(description: "Wait for completion")
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.append(Item(title: "baz1")) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "foo"),
         Item(title: "bar"),
         Item(title: "baz"),
@@ -36,10 +38,9 @@ class TestSpotsControllerManager: XCTestCase {
       Item(title: "baz1"),
       Item(title: "baz2")
     ]
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
+
     controller.append(items) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "foo"),
         Item(title: "bar"),
         Item(title: "baz"),
@@ -58,10 +59,8 @@ class TestSpotsControllerManager: XCTestCase {
       Item(title: "baz1"),
       Item(title: "baz2")
     ]
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.prepend(items) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "baz1"),
         Item(title: "baz2"),
         Item(title: "foo"),
@@ -76,10 +75,8 @@ class TestSpotsControllerManager: XCTestCase {
 
   func testInsertItem() {
     let expectation = self.expectation(description: "Wait for completion")
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.insert(Item(title: "baz1"), index: 1, componentIndex: 0) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "foo"),
         Item(title: "baz1"),
         Item(title: "bar"),
@@ -93,17 +90,15 @@ class TestSpotsControllerManager: XCTestCase {
 
   func testUpdateItem() {
     let expectation = self.expectation(description: "Wait for completion")
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.update(Item(title: "baz1"), index: 2, componentIndex: 0) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "foo"),
         Item(title: "bar"),
         Item(title: "baz1")
         ])
 
-      controller.update(Item(title: "baz1"), index: 2, componentIndex: 0) {
-        XCTAssertTrue(controller.components.first!.model.items == [
+      self.controller.update(Item(title: "baz1"), index: 2, componentIndex: 0) {
+        XCTAssertTrue(self.controller.components.first!.model.items == [
           Item(title: "foo"),
           Item(title: "bar"),
           Item(title: "baz1")
@@ -116,13 +111,11 @@ class TestSpotsControllerManager: XCTestCase {
 
   func testUpdateItemsWithIndexes() {
     let expectation = self.expectation(description: "Wait for completion")
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.components[0].model.items[0] = Item(title: "foo1")
     controller.components[0].model.items[1] = Item(title: "bar1")
 
     controller.update([0,1], componentIndex: 0) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "foo1"),
         Item(title: "bar1"),
         Item(title: "baz")
@@ -135,10 +128,8 @@ class TestSpotsControllerManager: XCTestCase {
 
   func testDeleteItem() {
     let expectation = self.expectation(description: "Wait for completion")
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.delete(1) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "foo"),
         Item(title: "baz")
         ])
@@ -151,10 +142,8 @@ class TestSpotsControllerManager: XCTestCase {
 
   func testDeleteItems() {
     let expectation = self.expectation(description: "Wait for completion")
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.delete([0,2]) {
-      XCTAssertTrue(controller.components.first!.model.items == [
+      XCTAssertTrue(self.controller.components.first!.model.items == [
         Item(title: "bar")
         ])
 
@@ -190,8 +179,6 @@ class TestSpotsControllerManager: XCTestCase {
 
   func testReloadController() {
     let expectation = self.expectation(description: "Wait for completion")
-    let controller = SpotsController(components: [component])
-    controller.prepareController()
     controller.reload() {
       expectation.fulfill()
     }
@@ -257,113 +244,104 @@ class TestSpotsControllerManager: XCTestCase {
 
     var expectation: XCTestExpectation? = self.expectation(description: "Wait for completion")
 
-    let items = [
-      Item(title: "foo"),
-      Item(title: "bar"),
-      Item(title: "baz")
-    ]
-    let model = ComponentModel(kind: .list, items: items)
-    let component = Component(model: model)
-    let controller = SpotsController(components: [component])
-
     controller.reloadIfNeeded([]) {
-      XCTAssertTrue(controller.components.isEmpty)
+      XCTAssertTrue(self.controller.components.isEmpty)
 
-      controller.reloadIfNeeded(initialModels) {
+      self.controller.reloadIfNeeded(initialModels) {
 
-        XCTAssertEqual(controller.components.count, 3)
+        XCTAssertEqual(self.controller.components.count, 3)
 
-        XCTAssertNotNil(controller.components[0].collectionView)
-        XCTAssertNotNil(controller.components[1].collectionView)
-        XCTAssertNotNil(controller.components[2].tableView)
+        XCTAssertNotNil(self.controller.components[0].collectionView)
+        XCTAssertNotNil(self.controller.components[1].collectionView)
+        XCTAssertNotNil(self.controller.components[2].tableView)
 
-        XCTAssertEqual(controller.components[0].model.items[0].title, "foo1")
-        XCTAssertEqual(controller.components[0].model.items[1].title, "bar1")
-        XCTAssertEqual(controller.components[0].model.items[2].title, "baz1")
+        XCTAssertEqual(self.controller.components[0].model.items[0].title, "foo1")
+        XCTAssertEqual(self.controller.components[0].model.items[1].title, "bar1")
+        XCTAssertEqual(self.controller.components[0].model.items[2].title, "baz1")
 
-        XCTAssertEqual(controller.components[1].model.items[0].title, "foo2")
-        XCTAssertEqual(controller.components[1].model.items[1].title, "bar2")
-        XCTAssertEqual(controller.components[1].model.items[2].title, "baz2")
+        XCTAssertEqual(self.controller.components[1].model.items[0].title, "foo2")
+        XCTAssertEqual(self.controller.components[1].model.items[1].title, "bar2")
+        XCTAssertEqual(self.controller.components[1].model.items[2].title, "baz2")
 
-        XCTAssertEqual(controller.components[2].model.items[0].title, "foo3")
-        XCTAssertEqual(controller.components[2].model.items[1].title, "bar3")
-        XCTAssertEqual(controller.components[2].model.items[2].title, "baz3")
+        XCTAssertEqual(self.controller.components[2].model.items[0].title, "foo3")
+        XCTAssertEqual(self.controller.components[2].model.items[1].title, "bar3")
+        XCTAssertEqual(self.controller.components[2].model.items[2].title, "baz3")
 
-        var carouselInstance = controller.components[0].collectionView
-        var gridInstance = controller.components[1].collectionView
-        var listInstance = controller.components[2].tableView
+        var carouselInstance = self.controller.components[0].collectionView
+        var gridInstance = self.controller.components[1].collectionView
+        var listInstance = self.controller.components[2].tableView
 
-        controller.reloadIfNeeded(updatedModels) {
-          XCTAssertEqual(controller.components.count, 3)
+        self.controller.reloadIfNeeded(updatedModels) {
+          XCTAssertEqual(self.controller.components.count, 3)
 
-          XCTAssertNotNil(controller.components[0].collectionView)
-          XCTAssertNotNil(controller.components[1].collectionView)
-          XCTAssertNotNil(controller.components[2].tableView)
+          XCTAssertNotNil(self.controller.components[0].collectionView)
+          XCTAssertNotNil(self.controller.components[1].collectionView)
+          XCTAssertNotNil(self.controller.components[2].tableView)
 
-          XCTAssertEqual(controller.components[0].model.items[0].title, "foo1")
-          XCTAssertEqual(controller.components[0].model.items[1].title, "bar4")
+          XCTAssertEqual(self.controller.components[0].model.items[0].title, "foo1")
+          XCTAssertEqual(self.controller.components[0].model.items[1].title, "bar4")
 
-          XCTAssertEqual(controller.components[1].model.items[0].title, "foo2")
-          XCTAssertEqual(controller.components[1].model.items[1].title, "bar2")
-          XCTAssertEqual(controller.components[1].model.items[2].title, "baz2")
-          XCTAssertEqual(controller.components[1].model.items[3].title, "baz4")
+          XCTAssertEqual(self.controller.components[1].model.items[0].title, "foo2")
+          XCTAssertEqual(self.controller.components[1].model.items[1].title, "bar2")
+          XCTAssertEqual(self.controller.components[1].model.items[2].title, "baz2")
+          XCTAssertEqual(self.controller.components[1].model.items[3].title, "baz4")
 
-          XCTAssertEqual(controller.components[2].model.items[0].title, "foo3")
-          XCTAssertEqual(controller.components[2].model.items[1].title, "bar3")
-          XCTAssertEqual(controller.components[2].model.items[2].title, "baz3")
+          XCTAssertEqual(self.controller.components[2].model.items[0].title, "foo3")
+          XCTAssertEqual(self.controller.components[2].model.items[1].title, "bar3")
+          XCTAssertEqual(self.controller.components[2].model.items[2].title, "baz3")
 
-          XCTAssertEqual(carouselInstance, controller.components[0].collectionView)
-          XCTAssertNotEqual(gridInstance, controller.components[1].collectionView)
-          XCTAssertEqual(listInstance, controller.components[2].tableView)
+          XCTAssertEqual(carouselInstance, self.controller.components[0].collectionView)
+          XCTAssertNotEqual(gridInstance, self.controller.components[1].collectionView)
+          XCTAssertEqual(listInstance, self.controller.components[2].tableView)
 
-          carouselInstance = controller.components[0].collectionView
-          gridInstance = controller.components[1].collectionView
-          listInstance = controller.components[2].tableView
+          carouselInstance = self.controller.components[0].collectionView
+          gridInstance = self.controller.components[1].collectionView
+          listInstance = self.controller.components[2].tableView
 
-          controller.reloadIfNeeded(updatedModels) {
-            XCTAssertEqual(controller.components.count, 3)
+          self.controller.reloadIfNeeded(updatedModels) {
+            XCTAssertEqual(self.controller.components.count, 3)
 
-            XCTAssertNotNil(controller.components[0].collectionView)
-            XCTAssertNotNil(controller.components[1].collectionView)
-            XCTAssertNotNil(controller.components[2].tableView)
+            XCTAssertNotNil(self.controller.components[0].collectionView)
+            XCTAssertNotNil(self.controller.components[1].collectionView)
+            XCTAssertNotNil(self.controller.components[2].tableView)
 
-            XCTAssertEqual(controller.components[0].model.items[0].title, "foo1")
-            XCTAssertEqual(controller.components[0].model.items[1].title, "bar4")
+            XCTAssertEqual(self.controller.components[0].model.items[0].title, "foo1")
+            XCTAssertEqual(self.controller.components[0].model.items[1].title, "bar4")
 
-            XCTAssertEqual(controller.components[1].model.items[0].title, "foo2")
-            XCTAssertEqual(controller.components[1].model.items[1].title, "bar2")
-            XCTAssertEqual(controller.components[1].model.items[2].title, "baz2")
-            XCTAssertEqual(controller.components[1].model.items[3].title, "baz4")
+            XCTAssertEqual(self.controller.components[1].model.items[0].title, "foo2")
+            XCTAssertEqual(self.controller.components[1].model.items[1].title, "bar2")
+            XCTAssertEqual(self.controller.components[1].model.items[2].title, "baz2")
+            XCTAssertEqual(self.controller.components[1].model.items[3].title, "baz4")
 
-            XCTAssertEqual(controller.components[2].model.items[0].title, "foo3")
-            XCTAssertEqual(controller.components[2].model.items[1].title, "bar3")
-            XCTAssertEqual(controller.components[2].model.items[2].title, "baz3")
+            XCTAssertEqual(self.controller.components[2].model.items[0].title, "foo3")
+            XCTAssertEqual(self.controller.components[2].model.items[1].title, "bar3")
+            XCTAssertEqual(self.controller.components[2].model.items[2].title, "baz3")
 
-            XCTAssertEqual(carouselInstance, controller.components[0].collectionView)
-            XCTAssertEqual(gridInstance, controller.components[1].collectionView)
-            XCTAssertEqual(listInstance, controller.components[2].tableView)
+            XCTAssertEqual(carouselInstance, self.controller.components[0].collectionView)
+            XCTAssertEqual(gridInstance, self.controller.components[1].collectionView)
+            XCTAssertEqual(listInstance, self.controller.components[2].tableView)
 
-            controller.reloadIfNeeded(finalUpdate) {
+            self.controller.reloadIfNeeded(finalUpdate) {
 
               guard expectation != nil else {
                 XCTFail("Exception is nil")
                 return
               }
 
-              XCTAssertEqual(controller.components.count, 2)
+              XCTAssertEqual(self.controller.components.count, 2)
 
-              XCTAssertNotNil(controller.components[0].collectionView)
-              XCTAssertNotNil(controller.components[1].tableView)
+              XCTAssertNotNil(self.controller.components[0].collectionView)
+              XCTAssertNotNil(self.controller.components[1].tableView)
 
-              XCTAssertEqual(controller.components[0].model.items[0].title, "foo1")
-              XCTAssertEqual(controller.components[0].model.items[1].title, "bar4")
+              XCTAssertEqual(self.controller.components[0].model.items[0].title, "foo1")
+              XCTAssertEqual(self.controller.components[0].model.items[1].title, "bar4")
 
-              XCTAssertEqual(controller.components[1].model.items[0].title, "foo3")
-              XCTAssertEqual(controller.components[1].model.items[1].title, "bar3")
-              XCTAssertEqual(controller.components[1].model.items[2].title, "baz3")
+              XCTAssertEqual(self.controller.components[1].model.items[0].title, "foo3")
+              XCTAssertEqual(self.controller.components[1].model.items[1].title, "bar3")
+              XCTAssertEqual(self.controller.components[1].model.items[2].title, "baz3")
 
-              XCTAssertEqual(carouselInstance, controller.components[0].collectionView)
-              XCTAssertNotEqual(listInstance, controller.components[1].tableView)
+              XCTAssertEqual(carouselInstance, self.controller.components[0].collectionView)
+              XCTAssertNotEqual(listInstance, self.controller.components[1].tableView)
 
               expectation?.fulfill()
               expectation = nil
