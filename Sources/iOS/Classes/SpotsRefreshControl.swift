5c96976932599bbab1da0634e743e04d28ce6714
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-04-20 09:47:43 +0200

    Improve iOS implementation when using reload with components
    afterUpdate will no longer call layout as that could cause the
    component to blink.
    
    SpotsController on iOS now uses a custom implementation of
    UIRefreshControl called SpotsRefreshControl. It is a tiny subclass of
    UIRefreshControl that overrides `endRefreshing()`. We do this to check
    that the refresh control is refreshing before calling
    `super.endRefreshing()`. If we don't do this, it could interrupted the
    scrolling as `endRefreshing` manipulates the scroll view it belongs to.
    
    SpotsControllerManager.reload(with changes ... moreItems) will not call
    completion using component.updateHeight, this is already covered by
    component internally.
    
    Component.updateHeightAndIndexes will now call refreshIndexes with
    completion as an argument.

---
+++ Sources/iOS/Classes/SpotsRefreshControl.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+/// A custom refresh control that makes sure that endRefreshing is only called
+/// when it is needed.
+class SpotsRefreshControl: UIRefreshControl {
+
+  // Must be explicitly called when the refreshing has completed
+  override func endRefreshing() {
+    // Only call endRefreshing if the refresh control is actually refreshing, otherwise
+    // scrolling will be interrupted.
+    if isRefreshing {
+      super.endRefreshing()
+    }
+  }
+}
