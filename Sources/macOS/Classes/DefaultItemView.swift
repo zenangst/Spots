c3eab19614318cfb14df4cf32382a500dba131a6
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-04-24 19:10:54 +0200

    Revert changes in DefaultItemView

--- Sources/macOS/Classes/DefaultItemView.swift
+++ Sources/macOS/Classes/DefaultItemView.swift
@@ -1,51 +1,54 @@
 import Cocoa
 
-open class DefaultItemView: View, ItemConfigurable, ViewStateDelegate {
+open class DefaultItemView: NSTableRowView, ItemConfigurable {
 
-  open override var isFlipped: Bool { return true }
+  override open var isFlipped: Bool {
+    return true
+  }
+
+  open override var isSelected: Bool {
+    didSet {
+      if isSelected {
+        layer?.backgroundColor = NSColor.black.withAlphaComponent(0.85).cgColor
+      } else {
+        layer?.backgroundColor = NSColor.black.cgColor
+      }
+    }
+  }
 
   open var preferredViewSize = Configuration.defaultViewSize
 
   lazy var titleLabel: NSTextField = {
-    let label = NSTextField()
-    label.isEditable = false
-    label.isSelectable = false
-    label.isBezeled = false
-    label.drawsBackground = false
+    let titleLabel = NSTextField()
+    titleLabel.isEditable = false
+    titleLabel.isSelectable = false
+    titleLabel.isBezeled = false
+    titleLabel.textColor = NSColor.white
+    titleLabel.drawsBackground = false
 
-    return label
+    return titleLabel
   }()
 
   lazy var subtitleLabel: NSTextField = {
-    let label = NSTextField()
-    label.isEditable = false
-    label.isSelectable = false
-    label.isBezeled = false
-    label.textColor = NSColor.black.withAlphaComponent(0.9)
-    label.drawsBackground = false
+    let subtitleLabel = NSTextField()
+    subtitleLabel.isEditable = false
+    subtitleLabel.isSelectable = false
+    subtitleLabel.isBezeled = false
+    subtitleLabel.textColor = NSColor.lightGray
+    subtitleLabel.drawsBackground = false
 
-    return label
-  }()
-
-  lazy var textLabel: NSTextField = {
-    let label = NSTextField()
-    label.isEditable = false
-    label.isSelectable = false
-    label.isBezeled = false
-    label.textColor = NSColor.black.withAlphaComponent(0.9)
-    label.drawsBackground = false
-
-    return label
+    return subtitleLabel
   }()
 
   lazy var lineView: NSView = {
-    let view = NSView()
-    view.frame.size.height = 1
-    view.wantsLayer = true
-    view.layer?.backgroundColor = NSColor.black.withAlphaComponent(0.4).cgColor
-    view.autoresizingMask = .viewWidthSizable
+    let lineView = NSView()
+    lineView.frame.size.height = 1
+    lineView.wantsLayer = true
+    lineView.layer = CALayer()
+    lineView.layer?.backgroundColor = NSColor.gray.withAlphaComponent(0.4).cgColor
+    lineView.autoresizingMask = .viewWidthSizable
 
-    return view
+    return lineView
   }()
 
   override init(frame frameRect: NSRect) {
@@ -53,67 +56,34 @@ open class DefaultItemView: View, ItemConfigurable, ViewStateDelegate {
 
     wantsLayer = true
     layer = CALayer()
-    layer?.backgroundColor = NSColor.white.cgColor
+    layer?.backgroundColor = NSColor.black.cgColor
 
     addSubview(titleLabel)
     addSubview(subtitleLabel)
-    addSubview(textLabel)
     addSubview(lineView)
-
-    setupConstraints()
   }
 
   public required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
-  func setupConstraints() {
-    titleLabel.translatesAutoresizingMaskIntoConstraints = false
-    subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
-    textLabel.translatesAutoresizingMaskIntoConstraints = false
-    lineView.translatesAutoresizingMaskIntoConstraints = false
-
-    titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
-    titleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
-    titleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
-
-    subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
-    subtitleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
-    subtitleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
-
-    textLabel.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 8).isActive = true
-    textLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
-    textLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
-    textLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
-
-    lineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
-    lineView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-    lineView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-    lineView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-  }
-
   open func configure( _ item: inout Item) {
     titleLabel.stringValue = item.title
+    titleLabel.frame.origin.x = 8
+
+    titleLabel.sizeToFit()
+    if !item.subtitle.isEmpty {
+      titleLabel.frame.origin.y = 8
+      titleLabel.font = NSFont.boldSystemFont(ofSize: 14)
+    } else {
+      titleLabel.frame.origin.y = item.size.height / 2 - titleLabel.frame.size.height / 2
+    }
+
+    subtitleLabel.frame.origin.x = 8
     subtitleLabel.stringValue = item.subtitle
-    textLabel.stringValue = item.text
+    subtitleLabel.sizeToFit()
+    subtitleLabel.frame.origin.y = titleLabel.frame.origin.y + subtitleLabel.frame.height
 
-    var height: CGFloat = 32
-    [titleLabel, subtitleLabel, textLabel].forEach {
-      let size = $0.sizeThatFits(item.size)
-      height += size.height
-    }
-
-    item.size.height = height
-  }
-
-  public func viewStateDidChange(_ viewState: ViewState) {
-    switch viewState {
-    case .highlighted:
-      layer?.backgroundColor = NSColor.gray.withAlphaComponent(0.3).cgColor
-    case .selected:
-      layer?.backgroundColor = NSColor.gray.withAlphaComponent(0.3).cgColor
-    case .normal:
-      layer?.backgroundColor = NSColor.white.cgColor
-    }
+    lineView.frame.origin.y = item.size.height + 1
   }
 }
