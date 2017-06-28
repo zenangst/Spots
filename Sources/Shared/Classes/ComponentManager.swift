f95abf3d9e5cff77134d5542d9aa241108faba2c
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-06-28 08:33:27 +0200

    Fix calling configureItem when appending the first item
    Fixes bug that first item does not appear after appending.

--- Sources/Shared/Classes/ComponentManager.swift
+++ Sources/Shared/Classes/ComponentManager.swift
@@ -27,12 +27,13 @@ public class ComponentManager {
     Dispatch.main { [weak self] in
       let numberOfItems = component.model.items.count
       component.model.items.append(item)
+      self?.itemManager.configureItem(at: numberOfItems, component: component)
 
       if numberOfItems == 0 {
-        component.userInterface?.reloadDataSource()
-        self?.finishComponentOperation(component, updateHeightAndIndexes: true, completion: completion)
+        component.userInterface?.reloadSection(0, withAnimation: animation) {
+          self?.finishComponentOperation(component, updateHeightAndIndexes: true, completion: completion)
+        }
       } else {
-        self?.itemManager.configureItem(at: numberOfItems, component: component, usesViewSize: true)
         component.userInterface?.insert([numberOfItems], withAnimation: animation) {
           self?.finishComponentOperation(component, updateHeightAndIndexes: true, completion: completion)
         }
