95d90781abb048233fca96f71b4746f1fcddeebf
Vadym Markov <markov.vadym@gmail.com>
2017-10-26 20:43:38 +0200

    Fix macOS target

--- Sources/macOS/Classes/Component.swift
+++ Sources/macOS/Classes/Component.swift
@@ -170,8 +170,7 @@ import Cocoa
   /// - Parameter cacheKey: The unique cache key that should be used for storing and restoring the component.
   public convenience init(cacheKey: String, configuration: Configuration = .shared) {
     let stateCache = StateCache(key: cacheKey)
-
-    self.init(model: ComponentModel(stateCache.load()), configuration: configuration)
+    self.init(model: stateCache.load() ?? .init(), configuration: configuration)
     self.stateCache = stateCache
   }
 
