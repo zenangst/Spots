aecc8af8a0d0dff359ab31847194439bbcd7776c
Vadym Markov <markov.vadym@gmail.com>
2017-11-13 21:54:54 +0100

    Fix indentation

--- Sources/Shared/Protocols/ItemModel.swift
+++ Sources/Shared/Protocols/ItemModel.swift
@@ -1,23 +1,23 @@
 import Foundation
 
 public protocol ItemCodable: Codable {
-    func equal(to rhs: ItemCodable) -> Bool
+  func equal(to rhs: ItemCodable) -> Bool
 }
 public protocol ItemModel: ItemCodable, Equatable {}
 
 public extension ItemCodable where Self: Equatable {
-    func equal(to rhs: ItemCodable) -> Bool {
-        guard let rhs = rhs as? Self else {
-            return false
-        }
-        return self == rhs
+  func equal(to rhs: ItemCodable) -> Bool {
+    guard let rhs = rhs as? Self else {
+      return false
     }
+    return self == rhs
+  }
 }
 
 extension ItemCodable {
-    func serialize() -> Data? {
-        let encoder = JSONEncoder()
-        let data = try? encoder.encode(self)
-        return data
-    }
+  func serialize() -> Data? {
+    let encoder = JSONEncoder()
+    let data = try? encoder.encode(self)
+    return data
+  }
 }
