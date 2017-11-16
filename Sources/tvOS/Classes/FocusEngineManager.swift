0578da4650b5bbfe652ba3f752e984f98b40b941
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-11-16 10:42:16 +0100

    Improve the FocusEngineManager when using grids

--- Sources/tvOS/Classes/FocusEngineManager.swift
+++ Sources/tvOS/Classes/FocusEngineManager.swift
@@ -25,6 +25,10 @@ class FocusEngineManager {
     let isFirstComponent = component.model.index == 0
     let firstRowItemIsFocused = component.model.kind == .carousel || itemIndex < Int(component.model.layout.span)
     let hasReachedTop = isFirstComponent && firstRowItemIsFocused
+    let windowHeight = component.view.window?.frame.size.height ?? 0.0
+    let componentWindowDiff = windowHeight - component.view.frame.size.height
+    let insets = CGFloat(component.model.layout.inset.top + component.model.layout.inset.bottom)
+    let hasReachedEnd = scrollView.contentSize.height - scrollView.frame.size.height - componentWindowDiff + insets
 
     if hasReachedTop {
       targetContentOffset.pointee.y = -contentInsetTop
@@ -40,7 +44,26 @@ class FocusEngineManager {
         if let spotsScrollView = scrollView as? SpotsScrollView,
           let yPosition = spotsScrollView.sizeCache[component.model.index] {
           result = yPosition + cell.frame.origin.y - contentInsetTop
+
+          if component.model.kind == .grid {
+            result -= CGFloat(component.model.layout.inset.top + component.model.layout.lineSpacing)
+          }
         }
+      }
+      targetContentOffset.pointee.y = result
+    } else if component.model.kind == .grid && direction == .down {
+      var result: CGFloat = 0
+      if let cell: UIView = component.userInterface!.cell(at: itemIndex) {
+        if let spotsScrollView = scrollView as? SpotsScrollView,
+          let yPosition = spotsScrollView.sizeCache[component.model.index] {
+          result = yPosition + cell.frame.origin.y - contentInsetTop
+          result -= CGFloat(component.model.layout.lineSpacing + component.model.layout.inset.top + component.model.layout.inset.bottom)
+        }
+      }
+
+      if result >= hasReachedEnd {
+        result = hasReachedEnd + componentWindowDiff
+        return
       }
 
       targetContentOffset.pointee.y = result
@@ -59,3 +82,4 @@ class FocusEngineManager {
     return contentInset
   }
 }
+
