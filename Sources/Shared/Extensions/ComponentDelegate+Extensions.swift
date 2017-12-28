eef519186055ee54e06c7db48fcaf1b9dedb6b08
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-12-28 13:42:58 +0100

    Add documentation

--- Sources/Shared/Extensions/ComponentDelegate+Extensions.swift
+++ Sources/Shared/Extensions/ComponentDelegate+Extensions.swift
@@ -14,10 +14,22 @@ public extension ComponentDelegate {
   func componentsDidChange(_ components: [Component]) {}
 
   #if os(tvOS)
+  /// A collection of strings that represent the contents of your component.
+  ///
+  /// - Parameter component: The component that owns the collection of strings.
+  /// - Returns: A collection of strings used for accelerated scrolling indexing.
   func componentIndexTitles(_ component: Component) -> [String]? {
     return nil
   }
 
+  /// The index path of the current indexed string.
+  ///
+  /// - Parameters:
+  ///   - component: The component that owns the items.
+  ///   - item: The item that is located at the index.
+  ///   - index: The index of the current item.
+  ///   - title: The title that the item is matched against.
+  /// - Returns: The index path used for focusing when using accelerated scrolling.
   func componentIndexPath(_ component: Component, item: Item, at index: Int, for title: String) -> IndexPath {
     return IndexPath(item: index, section: 0)
   }
