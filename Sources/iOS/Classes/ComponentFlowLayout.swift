82193f99fbdc822b8dd284c4b25b2bc21b9b495f
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-01-28 13:44:05 +0100

    Collect frames that intersect the current frame of the collection view

--- Sources/iOS/Classes/ComponentFlowLayout.swift
+++ Sources/iOS/Classes/ComponentFlowLayout.swift
@@ -14,6 +14,7 @@ open class ComponentFlowLayout: UICollectionViewFlowLayout {
   private var indexPathsToAnimate = [IndexPath]()
   private var indexPathsToMove = [IndexPath]()
   private var layoutAttributes: [UICollectionViewLayoutAttributes]?
+  private var intersectingAttributes: [UICollectionViewLayoutAttributes] = []
   private(set) var cachedFrames = [CGRect]()
 
   // Subclasses must override this method and use it to return the width and height of the collection view’s content. These values represent the width and height of all the content, not just the content that is currently visible. The collection view uses this information to configure its own content size to facilitate scrolling.
@@ -130,6 +131,7 @@ open class ComponentFlowLayout: UICollectionViewFlowLayout {
     var attributes = [UICollectionViewLayoutAttributes]()
     var nextX: CGFloat = sectionInset.left
     var nextY: CGFloat = 0.0
+    intersectingAttributes.removeAll()
 
     if let newAttributes = self.layoutAttributes {
       for (index, attribute) in newAttributes.enumerated() {
@@ -168,6 +170,11 @@ open class ComponentFlowLayout: UICollectionViewFlowLayout {
           } else {
             nextY = itemAttribute.frame.maxY + minimumLineSpacing
           }
+
+          if itemAttribute.frame.intersects(rect) {
+            intersectingAttributes.append(itemAttribute)
+          }
+
           attributes.append(itemAttribute)
         case .vertical:
           itemAttribute.frame.origin.y += component.headerHeight
@@ -176,6 +183,7 @@ open class ComponentFlowLayout: UICollectionViewFlowLayout {
           // Note that this only applies to vertical components.
           if itemAttribute.frame.intersects(rect) {
             attributes.append(itemAttribute)
+            intersectingAttributes.append(itemAttribute)
           }
         }
 
