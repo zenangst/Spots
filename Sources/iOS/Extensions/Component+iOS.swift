d5f27aa6fa6ae354fde2fba86caf089947fff8e9
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-02-02 14:01:52 +0100

    Refactor setupInfiniteScrolling to not rely on cached frames

--- Sources/iOS/Extensions/Component+iOS.swift
+++ Sources/iOS/Extensions/Component+iOS.swift
@@ -2,31 +2,23 @@ import UIKit
 
 extension Component {
   func setupInfiniteScrolling() {
-    guard let componentDataSource = componentDataSource,
-      model.items.count >= componentDataSource.buffer else {
+    guard let collectionView = collectionView,
+      let componentDataSource = componentDataSource,
+      model.items.count >= componentDataSource.buffer,
+      model.layout.infiniteScrolling,
+      let frame = collectionView.flowLayout?.layoutAttributesForItem(at: IndexPath(item: componentDataSource.buffer, section: 0))?.frame else {
         return
     }
 
-    let item = componentDataSource.buffer
     view.layoutIfNeeded()
     handleInfiniteScrolling()
-
-    guard let componentFlowLayout = collectionView?.flowLayout as? ComponentFlowLayout,
-      (item > 0 && item < componentFlowLayout.cachedFrames.count)
-      else {
-        return
-    }
-
-    let frame = componentFlowLayout.cachedFrames[item]
     let x: CGFloat
-
     switch model.interaction.paginate {
     case .page, .item:
       x = round(frame.origin.x - CGFloat(model.layout.inset.left))
     case .disabled:
       x = round(frame.origin.x - CGFloat(model.layout.itemSpacing * 1.5))
     }
-
-    collectionView?.setContentOffset(.init(x: x, y: 0), animated: false)
+    collectionView.setContentOffset(.init(x: x, y: 0), animated: false)
   }
 }
